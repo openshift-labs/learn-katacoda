e1009ac5711a76dc4b7f20d379d70fd34bc496dc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-02-13 11:40:31 +1100

    Copy resource file earlier in env-init.sh.

--- intro-openshift/resource-objects/env-init.sh
+++ intro-openshift/resource-objects/env-init.sh
@@ -12,7 +12,7 @@ echo '    }' >> route.json
 echo '  }' >> route.json
 echo '}' >> route.json
 scp route.json root@host01:~/route.json
+scp coming-soon-fqdn.json root@host01:~/coming-soon-fqdn.json
 ssh root@host01 "docker pull centos/python-35-centos7:latest"
 ssh root@host01 "docker pull openshiftroadshow/parksmap-katacoda:1.0.0"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
-scp coming-soon-fqdn.json root@host01:~/coming-soon-fqdn.json
