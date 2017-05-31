17e19854253809fb9dbef15aa76f5c78747fa483
Scott McCarty <smccarty@redhat.com>
2017-05-31 14:28:19 -0400

    SM: added docker pull for rhel7 image

--- intro-openshift/container-internals-lab-1/env-init.sh
+++ intro-openshift/container-internals-lab-1/env-init.sh
@@ -1,2 +1,3 @@
 ssh root@host01 'git clone https://github.com/fatherlinux/container-internals-lab.git; cp container-internals-lab/labs/lab-03/exercise-01/mega-proc.sh /usr/bin/mega-proc.sh'
+ssh root@host01 'docker pull rhel7'
 
