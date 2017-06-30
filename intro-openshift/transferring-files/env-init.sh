ebd7202dd56abb8096d26cbc384c600ba42f652f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-06-30 20:57:00 +1000

    Make host path volume writable.

--- intro-openshift/transferring-files/env-init.sh
+++ intro-openshift/transferring-files/env-init.sh
@@ -1,4 +1,5 @@
 ssh root@host01 "mkdir /tmp/pv-01 /tmp/pv-02"
+ssh root@host01 "chmod 0777 /tmp/pv-01 /tmp/pv-02"
 ssh root@host01 "oc create -f volumes.json"
 ssh root@host01 "docker pull openshiftkatacoda/blog-django-py:latest"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
