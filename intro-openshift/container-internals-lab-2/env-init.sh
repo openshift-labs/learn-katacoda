47cdaf75eed3041c20b084dd0ab9fbb967d625f3
Scott McCarty <smccarty@redhat.com>
2017-10-05 14:45:10 -0400

    SM: updated env and updated directory paths

--- intro-openshift/container-internals-lab-2/env-init.sh
+++ intro-openshift/container-internals-lab-2/env-init.sh
@@ -1,4 +1,5 @@
-ssh root@host01 'git clone https://github.com/fatherlinux/container-internals-lab.git; cp container-internals-lab/labs/lab-03/exercise-01/mega-proc.sh /usr/bin/mega-proc.sh'
+ssh root@host01 'git clone https://github.com/fatherlinux/intro-katacoda.git'
+ssh root@host01 'cp container-internals-lab-3/exercise-01/mega-proc.sh /usr/bin/mega-proc.sh'
 ssh root@host01 'docker pull rhel7'
 ssh root@host01 'docker pull rhel7-atomic'
 ssh root@host01 'docker pull nate/dockviz'
