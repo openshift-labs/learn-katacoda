04de5dd6945a78341d32c15820958d94c130a43a
Scott McCarty <smccarty@redhat.com>
2017-10-06 23:13:51 -0400

    SM: still hacking with start order

--- intro-openshift/container-internals-lab-3/set-env.sh
+++ intro-openshift/container-internals-lab-3/set-env.sh
@@ -1,9 +1,1 @@
-git clone https://github.com/fatherlinux/intro-katacoda.git
-sleep 5
-ln -s ~/intro-katacoda/intro-openshift/container-internals-lab-3/assets ~/
-cp ~/assets/exercise-01/mega-proc.sh /usr/bin/mega-proc.sh
-cp ~/assets/exercise-01/atomic-openshift-master.service /etc/systemd/system/atomic-openshift-master.service
-cp ~/assets/exercise-01/atomic-openshift-node.service /etc/systemd/system/atomic-openshift-node.service
-systemctl disable --now origin.service
-systemctl enable --now atomic-openshift-master.service; systemctl enable --now atomic-openshift-node.service
 ~/.launch.sh
