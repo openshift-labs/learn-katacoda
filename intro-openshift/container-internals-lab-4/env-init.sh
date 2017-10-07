812ed29b111f3c895521dab1a8f1fe7769257914
Scott McCarty <smccarty@redhat.com>
2017-10-07 16:17:52 -0400

    SM: added tools for exercise 2

--- intro-openshift/container-internals-lab-4/env-init.sh
+++ intro-openshift/container-internals-lab-4/env-init.sh
@@ -6,7 +6,7 @@ ssh root@host01 'git clone https://github.com/fatherlinux/wordpress-demo.git ~/a
 ssh root@host01 'cd ~/assets/exercise-01-b/; git checkout lab04-exercise01'
 ssh root@host01 '~/assets/exercise-01-b/create-pv.sh'
 ssh root@host01 'sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/assets/exercise-01-b/wordpress-objects.yaml'
-ssh root@host01 'yum install -y links'
+ssh root@host01 'yum install -y links httpd-tools'
 ssh root@host01 '/var/lib/openshift/openshift admin policy add-cluster-role-to-user cluster-admin admin'
 # ssh root@host01 'cp container-internals-lab-3/exercise-01/mega-proc.sh /usr/bin/mega-proc.sh'
 ssh root@host01 'docker pull rhel7'
