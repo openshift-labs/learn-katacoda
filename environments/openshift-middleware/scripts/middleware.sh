309ebdc02f3acad80d1973de57d07cc90c5994f6
Ben Hall <ben@benhall.me.uk>
2017-11-19 18:41:19 +0100

    Upgrade all the environments

--- environments/openshift-middleware/scripts/middleware.sh
+++ environments/openshift-middleware/scripts/middleware.sh
@@ -1,3 +1,10 @@
+# Bug in the Redhat Docker Image? 
+# https://bugzilla.redhat.com/show_bug.cgi?id=1510187#c2
+# https://www.mail-archive.com/users@lists.openshift.redhat.com/msg04484.html
+yum install python-rhsm-certificates -y
+mkdir -p /etc/docker/certs.d/registry.access.redhat.com/
+ln -s /etc/rhsm/ca/redhat-uep.pem /etc/docker/certs.d/registry.access.redhat.com/redhat-ca.crt
+
 yum install java-1.8.0-openjdk -y
 wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar xzf apache-maven-3.3.9-bin.tar.gz
@@ -10,6 +17,8 @@ alternatives --config mvn
 docker pull registry.access.redhat.com/jboss-eap-7/eap70-openshift
 docker pull registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift
 docker pull registry.access.redhat.com/jboss-fuse-6/fis-java-openshift
-docker pull registry.access.redhat.com/jboss-fuse-6/fis-karaf-openshift
 
-curl -L -o /opt/jboss-image-streams.json https://raw.githubusercontent.com/redhat-middleware-hackathon/openshift-files/master/jboss-image-streams.json
+# Requires RHEL subscription
+# docker pull registry.access.redhat.com/jboss-fuse-6/fis-karaf-openshift
+
+curl -k -L -o /opt/jboss-image-streams.json https://raw.githubusercontent.com/redhat-middleware-hackathon/openshift-files/master/jboss-image-streams.json
