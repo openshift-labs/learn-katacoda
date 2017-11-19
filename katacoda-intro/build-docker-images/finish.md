7619764a84ad42b1787d69a165a69e1b019c490b
Ben Hall <ben@benhall.me.uk>
2017-11-19 17:50:27 +0000

    Remove old scenarios

--- katacoda-intro/build-docker-images/finish.md
+++
@@ -1,1 +0,0 @@
-More information can be found at https://docs.openshift.org/latest/dev_guide/builds.html
--- katacoda-intro/playground/env-init.sh
+++
@@ -1,1 +0,0 @@
-ssh -i ~/.ssh/id_rsa root@host01 "curl -ksLO https://raw.githubusercontent.com/openshift/origin/master/examples/hello-openshift/hello-pod.json"
