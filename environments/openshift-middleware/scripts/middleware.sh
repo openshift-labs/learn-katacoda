223546ae3f0637e75737a5eacbdc2bc31aa573d1
Thomas Qvarnström <tqvarnst@redhat.com>
2017-12-04 22:40:47 +0100

    Added Eclipse Vert.x Scenario

--- environments/openshift-middleware/scripts/middleware.sh
+++ environments/openshift-middleware/scripts/middleware.sh
@@ -5,31 +5,11 @@ yum install python-rhsm-certificates -y
 mkdir -p /etc/docker/certs.d/registry.access.redhat.com/
 ln -s /etc/rhsm/ca/redhat-uep.pem /etc/docker/certs.d/registry.access.redhat.com/redhat-ca.crt
 
-yum install java-1.8.0-openjdk-devel -y
+yum install java-1.8.0-openjdk-devel tree -y
 wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar xzf apache-maven-3.3.9-bin.tar.gz
 rm -rf apache-maven-3.3.9-bin.tar.gz
 mkdir /usr/local/maven
 mv apache-maven-3.3.9/ /usr/local/maven/
 alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1
-alternatives --config mvn
 
-docker pull registry.access.redhat.com/jboss-eap-7/eap70-openshift
-docker pull registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift
-docker pull registry.access.redhat.com/jboss-fuse-6/fis-java-openshift
-docker pull registry.access.redhat.com/jboss-webserver-3/webserver31-tomcat8-openshift
-docker pull registry.access.redhat.com/jboss-decisionserver-6/decisionserver64-openshift
-docker pull registry.access.redhat.com/jboss-processserver-6/processserver64-openshift
-docker pull registry.access.redhat.com/jboss-datagrid-7/datagrid71-openshift
-docker pull registry.access.redhat.com/jboss-datagrid-7/datagrid71-client-openshift
-docker pull registry.access.redhat.com/jboss-datavirt-6/datavirt63-openshift
-docker pull registry.access.redhat.com/jboss-datavirt-6/datavirt63-driver-openshift
-docker pull registry.access.redhat.com/jboss-amq-6/amq63-openshift
-docker pull registry.access.redhat.com/redhat-sso-7/sso71-openshift
-docker pull bucharestgold/centos7-s2i-nodejs
-
-
-# Requires RHEL subscription
-# docker pull registry.access.redhat.com/jboss-fuse-6/fis-karaf-openshift
-
-curl -k -L -o /opt/jboss-image-streams.json https://raw.githubusercontent.com/redhat-middleware-hackathon/openshift-files/master/jboss-image-streams.json
