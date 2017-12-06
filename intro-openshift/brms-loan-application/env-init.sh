731de712ece96d42023ee4d3fb2b515911bd13df
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-06 11:29:30 +0100

    Automated image-stream and template installation. Automatically switching to developer OpenShift user on start.

---
+++ intro-openshift/brms-loan-application/env-init.sh
@@ -0,0 +1,4 @@
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift
+
+oc login -u developer -p developer
