95c500f0a7e6b044dc88e01764e9d6c931f01aa0
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-06 11:32:07 +0100

    Fixed env-init.sh

--- intro-openshift/brms-loan-application/env-init.sh
+++ intro-openshift/brms-loan-application/env-init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift
 oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift
 
