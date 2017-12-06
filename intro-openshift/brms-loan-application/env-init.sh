240ae6ef2de261907fb2f1c2528518c297ab2b95
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-06 11:53:09 +0100

    Moved is and template init to set-env.sh.

--- intro-openshift/brms-loan-application/env-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/local/bin/oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift > /root/log_is.log
-/usr/local/bin/oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift > /root/log_templates.log
-
-/usr/local/bin/oc login -u developer -p developer > /root/log_oc_change_user.log
--- intro-openshift/brms-loan-application/set-env.sh
+++ intro-openshift/brms-loan-application/set-env.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 ~/.launch.sh
+
+# Import the required ImageStreams and templates
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift
+
+# Logging as Developer
+/usr/local/bin/oc login -u developer -p developer
+
+echo "Katacoda BRMS Loan Application instance ready!"
