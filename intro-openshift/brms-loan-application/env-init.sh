1d166e2dd2b0eb0d9c1ea7dc992a35a494cba225
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-12-06 11:41:20 +0100

    Added logging to env-init.

--- intro-openshift/brms-loan-application/env-init.sh
+++ intro-openshift/brms-loan-application/env-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/local/bin/oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift
-/usr/local/bin/oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift
+/usr/local/bin/oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift > /root/log_is.log
+/usr/local/bin/oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/decisionserver/decisionserver64-basic-s2i.json -n openshift > /root/log_templates.log
 
-/usr/local/bin/oc login -u developer -p developer
+/usr/local/bin/oc login -u developer -p developer > /root/log_oc_change_user.log
