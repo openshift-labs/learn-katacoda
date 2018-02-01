6f70ad047150e040710c20ab8ce1e90f8a85b81d
Ben Hall <Ben@Benhall.me.uk>
2018-02-01 08:45:21 -0500

    Adding Istio bookinfo sample app @jamesfalkner

--- environments/openshift-middleware-3-7/scripts/istio-pull.sh
+++ environments/openshift-middleware-3-7/scripts/istio-pull.sh
@@ -1,4 +1,5 @@
 ISTIO=0.4.0
+BOOKINFO=0.2.8
 docker pull istio/istio-ca:$ISTIO
 docker pull istio/grafana:$ISTIO
 docker pull istio/pilot:$ISTIO
@@ -10,3 +11,10 @@ docker pull openzipkin/zipkin:latest
 docker pull jaegertracing/all-in-one:latest
 docker pull prom/prometheus:v2.0.0
 docker pull prom/statsd-exporter
+docker pull alpine
+docker pull istio/examples-bookinfo-ratings-v1:$BOOKINFO
+docker pull istio/examples-bookinfo-reviews-v2:$BOOKINFO
+docker pull istio/examples-bookinfo-reviews-v1:$BOOKINFO
+docker pull istio/examples-bookinfo-reviews-v3:$BOOKINFO
+docker pull istio/examples-bookinfo-details-v1:$BOOKINFO
+docker pull istio/examples-bookinfo-productpage-v1:$BOOKINFO
