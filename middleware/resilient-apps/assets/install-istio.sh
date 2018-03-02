3e2e7dbe2ce9a4d9a9b4fc8fd0cb46119d1de930
jamesfalkner <schtool@gmail.com>
2018-03-02 12:30:42 -0500

    update to istio 0.6.0 in resilent-apps scenario

--- middleware/resilient-apps/assets/install-istio.sh
+++ middleware/resilient-apps/assets/install-istio.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-ISTIO_VERSION=0.5.0
+ISTIO_VERSION=0.6.0
 ISTIO_HOME=${HOME}/istio-${ISTIO_VERSION}
 
 curl -kL https://git.io/getLatestIstio | sed 's/curl/curl -k /g' | ISTIO_VERSION=${ISTIO_VERSION} sh -
 export PATH="$PATH:${ISTIO_HOME}/bin"
 cd ${ISTIO_HOME}
-
-# workaround bug
-sed -i 's/mtlsExcludedServices/#mtlsExcludedServices/' install/kubernetes/istio.yaml
 
 oc new-project istio-system
 oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account
@@ -17,16 +14,22 @@ oc adm policy add-scc-to-user anyuid -z istio-egress-service-account
 oc adm policy add-scc-to-user privileged -z istio-egress-service-account
 oc adm policy add-scc-to-user anyuid -z istio-pilot-service-account
 oc adm policy add-scc-to-user privileged -z istio-pilot-service-account
+oc adm policy add-scc-to-user anyuid -z istio-grafana-service-account -n istio-system
+oc adm policy add-scc-to-user anyuid -z istio-prometheus-service-account -n istio-system
+oc adm policy add-scc-to-user anyuid -z prometheus -n istio-system
+oc adm policy add-scc-to-user privileged -z prometheus
+oc adm policy add-scc-to-user anyuid -z grafana -n istio-system
+oc adm policy add-scc-to-user privileged -z grafana
 oc adm policy add-scc-to-user anyuid -z default
 oc adm policy add-scc-to-user privileged -z default
 oc adm policy add-cluster-role-to-user cluster-admin -z default
+
 oc apply -f install/kubernetes/istio.yaml
 oc create -f install/kubernetes/addons/prometheus.yaml
 oc create -f install/kubernetes/addons/grafana.yaml
 oc create -f install/kubernetes/addons/servicegraph.yaml
-# workaround go bug
-oc get deployment servicegraph -n istio-system -o json | sed 's/servicegraph:0.5.0/servicegraph:0.5.1/g' | oc replace -f - -n istio-system
 oc apply -f https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml
+
 oc expose svc grafana
 oc expose svc servicegraph
 oc expose svc jaeger-query
