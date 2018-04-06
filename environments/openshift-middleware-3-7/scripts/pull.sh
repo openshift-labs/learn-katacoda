ce5c07f0fe52669c9bee9c2b881d98f0cc502f8a
Justin Lee <julee@redhat.com>
2018-04-06 15:30:16 -0400

    add pulls to fetch owsk-openshift images locally to reduce wait times

--- environments/openshift-middleware-3-7/scripts/pull.sh
+++ environments/openshift-middleware-3-7/scripts/pull.sh
@@ -22,3 +22,24 @@ docker pull centos/python-35-centos7:latest
 docker pull katacoda/contained-nfs-server:centos7
 
 docker pull openshiftroadshow/parksmap-katacoda:1.0.0
+
+###  OpenWhisk images
+PROJECTODD_VERSION=cc60dfe
+OPENWHISK_VERSION=rhdemo-b7724ef
+docker pull busybox
+docker pull centos/nginx-112-centos7@sha256:42330f7f29ba1ad67819f4ff3ae2472f62de13a827a74736a5098728462212e7
+docker pull openwhisk/alarmprovider:1.9.0
+docker pull projectodd/action-java-8:${PROJECTODD_VERSION}
+docker pull projectodd/action-nodejs-6:${PROJECTODD_VERSION}
+docker pull projectodd/action-nodejs-8:${PROJECTODD_VERSION}
+docker pull projectodd/action-php-7:${PROJECTODD_VERSION}
+docker pull projectodd/action-python-2:${PROJECTODD_VERSION}
+docker pull projectodd/action-python-3:${PROJECTODD_VERSION}
+docker pull projectodd/controller:${OPENWHISK_VERSION}
+docker pull projectodd/invoker:${OPENWHISK_VERSION}
+docker pull projectodd/whisk_alarms:${PROJECTODD_VERSION}
+docker pull projectodd/whisk_catalog:da00e0c
+docker pull projectodd/whisk_couchdb:${PROJECTODD_VERSION}
+docker pull strimzi/cluster-controller:latest
+docker pull strimzi/kafka:latest
+docker pull strimzi/zookeeper:latest
