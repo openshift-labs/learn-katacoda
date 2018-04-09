614b6a15516668bb00758d43ac05377a8f493d1e
Marek Jelen <marek@jelen.biz>
2018-04-09 09:58:52 +0200

    Update structure

--- introduction/developing-with-odo/env-init.sh
+++
@@ -1,4 +0,0 @@
-export ODO_VERSION="0.0.3"
-
-ssh root@host01 "curl -k -L \"https://github.com/redhat-developer/ocdev/releases/download/v${ODO_VERSION}/ocdev-linux-amd64.gz\" | gzip -d > /usr/local/bin/ocdev; chmod +x /usr/local/bin/ocdev"
-ssh root@host01 "echo 'source <(ocdev completion bash)' >> .bashrc"
--- introduction/developing-with-odo/set-env.sh
+++ introduction/developing-with-odo/set-env.sh
@@ -1,3 +1,8 @@
 ~/.launch.sh
 
+ODO_VERSION="0.0.3"
+
+curl -k -L "https://github.com/redhat-developer/ocdev/releases/download/v${ODO_VERSION}/ocdev-linux-amd64.gz" | gzip -d > /usr/local/bin/ocdev; chmod +x /usr/local/bin/ocdev
+echo 'source <(ocdev completion bash)' >> .bashrc
+
 exec /bin/bash
