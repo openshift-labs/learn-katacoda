18e37940421b3af37bbbfebccd6e79b1ac133bb9
Justin Lee <julee@redhat.com>
2018-04-16 13:03:18 -0400

    update to a specific tag for versions. break out owsk-pull.sh to a separate script

--- environments/openshift-middleware-3-7/scripts/pull.sh
+++ environments/openshift-middleware-3-7/scripts/pull.sh
@@ -22,27 +22,3 @@ docker pull centos/python-35-centos7:latest
 docker pull katacoda/contained-nfs-server:centos7
 
 docker pull openshiftroadshow/parksmap-katacoda:1.0.0
-
-###  OpenWhisk images
-PROJECTODD_VERSION=f5eae82
-OPENWHISK_VERSION=rhdemo-b7724ef
-STRIMZI_VERSION=0.2.0
-docker pull busybox
-docker pull centos/nginx-112-centos7@sha256:42330f7f29ba1ad67819f4ff3ae2472f62de13a827a74736a5098728462212e7
-docker pull openwhisk/alarmprovider:1.9.0
-docker pull projectodd/action-java-8:${PROJECTODD_VERSION}
-docker pull projectodd/action-nodejs-6:${PROJECTODD_VERSION}
-docker pull projectodd/action-nodejs-8:${PROJECTODD_VERSION}
-docker pull projectodd/action-php-7:${PROJECTODD_VERSION}
-docker pull projectodd/action-python-2:${PROJECTODD_VERSION}
-docker pull projectodd/action-python-3:${PROJECTODD_VERSION}
-docker pull projectodd/controller:${OPENWHISK_VERSION}
-docker pull projectodd/invoker:${OPENWHISK_VERSION}
-docker pull projectodd/whisk_alarms:${PROJECTODD_VERSION}
-docker pull projectodd/whisk_catalog:da00e0c
-docker pull projectodd/whisk_couchdb:${PROJECTODD_VERSION}
-docker pull strimzi/cluster-controller:${STRIMZI_VERSION}
-docker pull strimzi/kafka:${STRIMZI_VERSION}
-docker pull strimzi/zookeeper:${STRIMZI_VERSION}
-
-curl -o /opt/openwhisk-template https://git.io/openwhisk-template
