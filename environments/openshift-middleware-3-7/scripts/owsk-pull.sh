15791dc8fa583d4d59b031dd07c9753ec7a298c4
Justin Lee <julee@redhat.com>
2018-04-16 13:03:18 -0400

    update to a specific tag for versions. break out owsk-pull.sh to a separate script

---
+++ environments/openshift-middleware-3-7/scripts/owsk-pull.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+###  OpenWhisk images
+PROJECTODD_LEARN_TAG=learn
+docker pull projectodd/busybox:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/nginx:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/alarmprovider:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-java-8:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-nodejs-6:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-nodejs-8:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-php-7:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-python-2:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-python-3:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/action-swift-3:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/controller:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/invoker:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/whisk_alarms:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/whisk_catalog:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/whisk_couchdb:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/cluster-controller:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/kafka:${PROJECTODD_LEARN_TAG}
+docker pull projectodd/zookeeper:${PROJECTODD_LEARN_TAG}
