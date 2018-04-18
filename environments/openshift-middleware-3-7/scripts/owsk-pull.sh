bf38694f779838b8d23436a99170ab34852a54df
Justin Lee <julee@redhat.com>
2018-04-18 10:52:43 -0400

    roll back to prior versions

--- environments/openshift-middleware-3-7/scripts/owsk-pull.sh
+++ environments/openshift-middleware-3-7/scripts/owsk-pull.sh
@@ -1,27 +1,22 @@
-#!/usr/bin/env bash
-
 ###  OpenWhisk images
-PROJECTODD_LEARN_TAG=learn
-docker pull projectodd/busybox:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/nginx:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/alarmprovider:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-java-8:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-nodejs-6:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-nodejs-8:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-php-7:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-python-2:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-python-3:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/action-swift-3:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/controller:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/invoker:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/whisk_alarms:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/whisk_catalog:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/whisk_couchdb:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/cluster-controller:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/kafka:${PROJECTODD_LEARN_TAG}
-docker pull projectodd/zookeeper:${PROJECTODD_LEARN_TAG}
+PROJECTODD_VERSION=cc60dfe
+OPENWHISK_VERSION=rhdemo-b7724ef
+STRIMZI_VERSION=0.2.0
 
-# leave both in for now until the learn template gets merged
-# the scenarios can reference /opt/openwhisk-template in the meantime
-curl -o /opt/openwhisk-template https://raw.githubusercontent.com/projectodd/openwhisk-openshift/master/template.yml
-curl -o /opt/openwhisk-learn-template https://raw.githubusercontent.com/projectodd/openwhisk-openshift/master/learn-template.yml
+docker pull busybox
+docker pull centos/nginx-112-centos7@sha256:42330f7f29ba1ad67819f4ff3ae2472f62de13a827a74736a5098728462212e7
+docker pull openwhisk/alarmprovider:1.9.0
+docker pull projectodd/action-java-8:${PROJECTODD_VERSION}
+docker pull projectodd/action-nodejs-6:${PROJECTODD_VERSION}
+docker pull projectodd/action-nodejs-8:${PROJECTODD_VERSION}
+docker pull projectodd/action-php-7:${PROJECTODD_VERSION}
+docker pull projectodd/action-python-2:${PROJECTODD_VERSION}
+docker pull projectodd/action-python-3:${PROJECTODD_VERSION}
+docker pull projectodd/controller:${OPENWHISK_VERSION}
+docker pull projectodd/invoker:${OPENWHISK_VERSION}
+docker pull projectodd/whisk_alarms:${PROJECTODD_VERSION}
+docker pull projectodd/whisk_catalog:da00e0c
+docker pull projectodd/whisk_couchdb:${PROJECTODD_VERSION}
+docker pull strimzi/cluster-controller:${STRIMZI_VERSION}
+docker pull strimzi/kafka:${STRIMZI_VERSION}
+docker pull strimzi/zookeeper:${STRIMZI_VERSION}
