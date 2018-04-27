942ba305b713bd829f70541d346911a6be5b73e6
Justin Lee <evanchooly@users.noreply.github.com>
2018-04-27 11:42:03 -0400

    More openwhisk optimizations (#181)
    * add the action-switf-3 image pull
    pre-run the maven archetype install earlier
    
    * First Serverless Scenario (#183)
    
    * Update homepage-pathway.json
    
    Add serverless group
    
    * Create 00-intro.md
    
    * Create 01-greeting-function.md
    
    * Create 99-outro.md
    
    * Create index.json
    
    * Create pre-scenario-setup.sh
    
    * Create serverless-pathway.json
    
    * Create set-env.sh
    
    * Delete pre-scenario-setup.sh
    
    * Update index.json
    
    * Create run-init.sh
    
    * Create setup-environment.sh
    
    * add web action scenario
    
    * revert commit.  clashes with another PR.

--- environments/openshift-middleware-3-7/scripts/owsk-install.sh
+++ environments/openshift-middleware-3-7/scripts/owsk-install.sh
@@ -5,23 +5,22 @@ cd /tmp
     wget -N -nv https://github.com/apache/incubator-openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz
 [ -f /usr/local/bin/wsk ] || sudo tar xzvf OpenWhisk_CLI-latest-linux-386.tgz -C /usr/local/bin wsk
 
-until $(oc status &> /dev/null); do sleep 1; done; oc adm policy add-cluster-role-to-user cluster-admin admin
-oc new-project faas --display-name="FaaS - Apache OpenWhisk"
-oc adm policy add-role-to-user admin developer -n faas
-
-oc process -f https://git.io/openwhisk-template | oc create -f -
 
 git clone https://github.com/apache/incubator-openwhisk-devtools /tmp/openwhisk-devtools
-
 cd /tmp/openwhisk-devtools/java-action-archetype \
     && mvn -DskipTests clean install  \
     && cd  \
     && rm -rf /tmp/openwhisk-devtools
 
-while [ -z "`oc logs controller-0 -n faas 2>&1 | grep "invoker status changed"`" ]
-do
-    echo "Waiting for OpenWhisk to finish initializing (`date`)"
-    sleep 10
-done
+#until $(oc status &> /dev/null); do sleep 1; done; oc adm policy add-cluster-role-to-user cluster-admin admin
+#oc new-project faas --display-name="FaaS - Apache OpenWhisk"
+#oc adm policy add-role-to-user admin developer -n faas
+#
+#oc process -f https://git.io/openwhisk-template | oc create -f -
+#while [ -z "`oc logs controller-0 -n faas 2>&1 | grep "invoker status changed"`" ]
+#do
+#    echo "Waiting for OpenWhisk to finish initializing (`date`)"
+#    sleep 10
+#done
 
-oc patch route openwhisk --namespace faas -p '{"spec":{"tls": {"insecureEdgeTerminationPolicy": "Allow"}}}'
+#oc patch route openwhisk --namespace faas -p '{"spec":{"tls": {"insecureEdgeTerminationPolicy": "Allow"}}}'
