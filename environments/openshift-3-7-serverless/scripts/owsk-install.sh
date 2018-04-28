341f7cb7a7b9dd6dc4e82da31a09df9441f40d46
Jim Curtis <jim.curtis@redhat.com>
2018-04-28 12:31:17 -0700

    Create owsk-install.sh

---
+++ environments/openshift-3-7-serverless/scripts/owsk-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd /tmp
+[ -f OpenWhisk_CLI-latest-linux-386.tgz ] || \
+    wget -N -nv https://github.com/apache/incubator-openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz
+[ -f /usr/local/bin/wsk ] || sudo tar xzvf OpenWhisk_CLI-latest-linux-386.tgz -C /usr/local/bin wsk
+
+
+git clone https://github.com/apache/incubator-openwhisk-devtools /tmp/openwhisk-devtools
+cd /tmp/openwhisk-devtools/java-action-archetype \
+    && mvn -DskipTests clean install  \
+    && cd  \
+    && rm -rf /tmp/openwhisk-devtools
+
+
+
+until $(oc status &> /dev/null); do sleep 1; done; oc adm policy add-cluster-role-to-user cluster-admin admin
+
+mkdir -p /root/openwhisk/bin/"
+wget -c https://github.com/apache/incubator-openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz -P /root/installation
+tar -zxvf /root/installation/OpenWhisk_CLI-latest-linux-386.tgz -C /root/openwhisk/bin/
+oc new-project faas --display-name='FaaS - OpenShift Cloud Functions'
+oc adm policy add-role-to-user admin developer -n faas
+oc process -f https://git.io/vpnUR | oc create -f -
+
+while $(oc get pods -n faas controller-0 | grep 0/1 > /dev/null); do sleep 1; done
+while [ -z "`oc logs controller-0 -n faas 2>&1 | grep "invoker status changed"`" ]
+do
+    sleep 5
+done
+
+oc patch route openwhisk --namespace faas -p '{"spec":{"tls": {"insecureEdgeTerminationPolicy": "Allow"}}}'
