a06776e4541ec49dbad88bb6472b0c1cf26bb0fa
Jim Curtis <jim.curtis@redhat.com>
2018-04-28 17:09:13 -0700

    Update owsk-install.sh

--- environments/openshift-3-7-serverless/scripts/owsk-install.sh
+++ environments/openshift-3-7-serverless/scripts/owsk-install.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-cd /tmp
-[ -f OpenWhisk_CLI-latest-linux-386.tgz ] || \
-    wget -N -nv https://github.com/apache/incubator-openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz
-[ -f /usr/local/bin/wsk ] || sudo tar xzvf OpenWhisk_CLI-latest-linux-386.tgz -C /usr/local/bin wsk
-
-
 git clone https://github.com/apache/incubator-openwhisk-devtools /tmp/openwhisk-devtools
 cd /tmp/openwhisk-devtools/java-action-archetype \
     && mvn -DskipTests clean install  \
     && cd  \
     && rm -rf /tmp/openwhisk-devtools
-
-
 
 until $(oc status &> /dev/null); do sleep 1; done; oc adm policy add-cluster-role-to-user cluster-admin admin
 
