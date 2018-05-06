610b4cc1c7ff805f9edc9f08eb152df5222a561a
Grant Shipley <gshipley@gmail.com>
2018-05-06 19:13:30 -0400

    Added imagestreams

--- introduction/developing-with-odo/set-env.sh
+++ introduction/developing-with-odo/set-env.sh
@@ -1,48 +1,11 @@
 ~/.launch.sh
 
-export ODO_VERSION="0.0.5"
-
-curl -k -L "https://dl.bintray.com/odo/odo/latest/linux-amd64/odo" > /usr/local/bin/odo; chmod +x /usr/local/bin/odo
-#curl -k -L "https://github.com/redhat-developer/odo/releases/download/v${ODO_VERSION}/odo-linux-amd64.gz" | gzip -d > /usr/local/bin/odo; chmod +x /usr/local/bin/odo
-echo 'source <(odo completion bash)' >> .bashrc
-
-yum install -y bash-completion
-
-git clone https://github.com/marekjelen/katacoda-odo-backend.git backend
-git clone https://github.com/marekjelen/katacoda-odo-frontend.git frontend
-
 oc adm policy add-cluster-role-to-user cluster-admin developer
-
-for i in {1..20}; do
-cat <<EOF > tmp.json
-{
-    "apiVersion": "v1",
-    "kind": "PersistentVolume",
-    "metadata": {
-        "name": "pv-${i}"
-    },
-    "spec": {
-        "accessModes": [
-            "ReadWriteOnce",
-            "ReadWriteMany",
-            "ReadOnlyMany"
-        ],
-        "capacity": {
-            "storage": "10Gi"
-        },
-        "hostPath": {
-            "path": "/data/pv-${i}"
-        },
-        "persistentVolumeReclaimPolicy": "Recycle"
-    }
-}
-EOF
-mkdir -p /data/pv-$i
-chmod 0777 /data/pv-$i
-oc create -f tmp.json
-rm -rf tmp.json
-done
-
 oc new-project myproject
 
-exec /bin/bash
+oc apply -n openshift -f https://gist.githubusercontent.com/kadel/bb5c86158bfc44e335ea99c91fab04e3/raw/my-wildfly-is.yaml
+oc apply -n openshift -f https://gist.githubusercontent.com/kadel/ee43d945013931c43cab5d8280847102/raw/my-php-is.yaml
+
+# download images
+oc import-image -n openshift wildfly
+oc import-image -n openshift php
