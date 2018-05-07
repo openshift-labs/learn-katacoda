64262bed0dd90c0f4326df873a8f236e94e324a7
Jorge Morales <jorgemoralespou@gmail.com>
2018-05-07 12:32:49 -0700

    Updated script to prevent error output

--- introduction/developing-with-odo/assets/.configure-odo.sh
+++ introduction/developing-with-odo/assets/.configure-odo.sh
@@ -2,8 +2,8 @@ echo "Configuring scenario"
 oc adm policy add-cluster-role-to-user cluster-admin developer 1> /dev/null
 oc new-project myproject > /dev/null 2>&1
 
-oc apply -n openshift -f https://gist.githubusercontent.com/kadel/bb5c86158bfc44e335ea99c91fab04e3/raw/my-wildfly-is.yaml 1> /dev/null
-oc apply -n openshift -f https://gist.githubusercontent.com/kadel/ee43d945013931c43cab5d8280847102/raw/my-php-is.yaml 1> /dev/null
+oc replace -n openshift -f https://gist.githubusercontent.com/kadel/bb5c86158bfc44e335ea99c91fab04e3/raw/my-wildfly-is.yaml 1> /dev/null
+oc replace -n openshift -f https://gist.githubusercontent.com/kadel/ee43d945013931c43cab5d8280847102/raw/my-php-is.yaml 1> /dev/null
 
 # download images
 oc import-image -n openshift wildfly 1> /dev/null
