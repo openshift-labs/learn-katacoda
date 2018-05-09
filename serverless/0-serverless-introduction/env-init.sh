38ef0c962425389dd8a03c584a292d07692988fd
Ben Hall <ben@benhall.me.uk>
2018-05-09 13:07:59 +0100

    wget and tree is in the base image

--- serverless/0-serverless-introduction/env-init.sh
+++ serverless/0-serverless-introduction/env-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ssh root@host01 "rm -rf /root/projects"
 ssh root@host01 "mkdir -p /root/projects/ocf && mkdir -p /root/openwhisk/bin/"
-ssh root@host01 "yum -y install wget tree"
 ssh root@host01 "mkdir -p ~/root/projects/getting-started"
