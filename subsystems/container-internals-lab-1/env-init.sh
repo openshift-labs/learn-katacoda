e6c2d1e24c6b18ac1c5a3eec36bba3180b6fb44a
Scott McCarty <smccarty@redhat.com>
2018-05-15 11:21:16 -0400

    SM: moved image pulls to end

--- subsystems/container-internals-lab-1/env-init.sh
+++ subsystems/container-internals-lab-1/env-init.sh
@@ -1,8 +1,5 @@
 # General Preparation
 ssh root@host01 'git clone https://github.com/fatherlinux/container-internals-lab.git ~/labs'
-ssh root@host01 'docker pull rhel7 &'
-ssh root@host01 'docker pull rhel7-atomic &'
-ssh root@host01 'docker pull nate/dockviz &'
 ssh root@host01 '/var/lib/openshift/openshift admin policy add-cluster-role-to-user cluster-admin admin'
 
 # Lab 1
@@ -17,3 +14,8 @@ ssh root@host01 'cd ~/labs/lab2-step4/; git checkout centos7'
 
 # Lab 4
 ssh root@host01 'git clone https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab4-step4/'
+
+# Final Preparation
+ssh root@host01 'docker pull rhel7'
+ssh root@host01 'docker pull rhel7-atomic'
+ssh root@host01 'docker pull nate/dockviz'
