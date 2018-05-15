1333b5d4c668735477c742b95462ea2a1e0edfb9
Scott McCarty <smccarty@redhat.com>
2018-05-15 11:33:37 -0400

    SM: updated checkouts again

--- subsystems/container-internals-lab-1/env-init.sh
+++ subsystems/container-internals-lab-1/env-init.sh
@@ -1,18 +1,18 @@
 # General Preparation
-ssh root@host01 'git clone https://github.com/fatherlinux/container-internals-lab.git ~/labs'
+ssh root@host01 'git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs'
 ssh root@host01 '/var/lib/openshift/openshift admin policy add-cluster-role-to-user cluster-admin admin'
 
 # Lab 1
 ssh root@host01 'cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh'
 
 # Lab 2
-ssh root@host01 'git clone --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/'
+ssh root@host01 'git clone --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/'
 
 # Lab 3
 
 
 # Lab 4
-ssh root@host01 'git clone https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab4-step4/'
+#ssh root@host01 'git clone --depth 1 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab4-step4/'
 
 # Final Preparation
 ssh root@host01 'docker pull rhel7'
