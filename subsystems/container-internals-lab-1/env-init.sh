522c9b45bbf77428ed531866cd629568bfcf4bcc
Scott McCarty <smccarty@redhat.com>
2018-05-15 14:40:46 -0400

    SM: fixed rpm install, updated branch logic

--- subsystems/container-internals-lab-1/env-init.sh
+++ subsystems/container-internals-lab-1/env-init.sh
@@ -15,10 +15,10 @@ ssh root@host01 'systemctl enable --now atomic-openshift-master.service; systemc
 ssh root@host01 'rpm -ivh http://mirror.centos.org/centos/7/os/x86_64/Packages/strace-4.12-6.el7.x86_64.rpm'
 
 # Lab 4
-ssh root@host01 'git clone --depth 1 --single-branch --branch lab04-exercise01 https://github.com/fatherlinux/wordpress-demo.git ~/labs/lab4-step1'
-ssh root@host01 '~/labs/lab4-step1/create-pv.sh'
+ssh root@host01 'git clone --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/lab4-step1'
+ssh root@host01 '~/labs/lab4-step1/create.sh'
 ssh root@host01 'sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/lab4-step1/wordpress-objects.yaml'
-ssh root@host01 'rpm -ivh http://mirror.centos.org/centos/7/os/x86_64/Packages/httpd-tools-2.4.6-80.el7.centos.x86_64.rpm'
+ssh root@host01 'rpm -ivh http://mirror.centos.org/centos/7/os/x86_64/Packages/httpd-tools-2.4.6-80.el7.centos.x86_64.rpm http://mirror.centos.org/centos/7/os/x86_64/Packages/apr-1.4.8-3.el7_4.1.x86_64.rpm http://mirror.centos.org/centos/7/os/x86_64/Packages/apr-util-1.5.2-6.el7.x86_64.rpm'
 
 # Final Preparation
 ssh root@host01 'docker pull rhel7'
