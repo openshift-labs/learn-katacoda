5f706fb2a56b1feabb55f66ece94c8e54242b8d7
Scott McCarty <smccarty@redhat.com>
2018-05-15 06:05:47 -0700

    SM: Updated env-init to work better in reserved env

--- subsystems/container-internals-lab-1/env-init.sh
+++ subsystems/container-internals-lab-1/env-init.sh
@@ -1,13 +1,16 @@
 # General Preparation
-ssh root@host01 'mkdir ~/labs'
+ssh root@host01 'git clone https://github.com/fatherlinux/container-internals-lab.git ~/labs'
 ssh root@host01 'docker pull rhel7'
 ssh root@host01 'docker pull rhel7-atomic'
 ssh root@host01 'docker pull nate/dockviz'
 ssh root@host01 '/var/lib/openshift/openshift admin policy add-cluster-role-to-user cluster-admin admin'
 
 # Lab 1
+ssh root@host01 'cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh'
 
-ssh root@host01 'cp ~/intro-katacoda/subsystems/container-internals-lab-3/assets/exercise-01/mega-proc.sh /usr/bin/mega-proc.sh'
+# Lab 2
+
+# Lab 3
 
 # Lab 4
 ssh root@host01 'git clone https://github.com/fatherlinux/container-supply-chain.git ~/labs/e04/'
