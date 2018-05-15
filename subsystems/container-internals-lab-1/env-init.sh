ad16642dcd4004e64dfed925b5767e5fad61ca49
Scott McCarty <smccarty@redhat.com>
2018-05-15 10:50:45 -0400

    SM: fixed branch checkout

--- subsystems/container-internals-lab-1/env-init.sh
+++ subsystems/container-internals-lab-1/env-init.sh
@@ -9,8 +9,7 @@ ssh root@host01 '/var/lib/openshift/openshift admin policy add-cluster-role-to-u
 ssh root@host01 'cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh'
 
 # Lab 2
-ssh root@host01 'git clone https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/'
-ssh root@host01 'cd ~/labs/lab1-step4/; git branch centos7'
+ssh root@host01 'git clone -b centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/'
 
 # Lab 3
 
