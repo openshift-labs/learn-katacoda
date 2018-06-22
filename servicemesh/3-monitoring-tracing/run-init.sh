ca5dc8b9e0d26fd730e737d24ab9066c140cfb12
Rafael Benevides <benevides@redhat.com>
2018-06-22 10:39:58 +0200

    Increase wait time (#219)

--- servicemesh/3-monitoring-tracing/run-init.sh
+++ servicemesh/3-monitoring-tracing/run-init.sh
@@ -6,7 +6,7 @@ ssh root@host01 "rm -rf /root/projects/rhoar-getting-started /root/temp-pom.xml"
 
 ssh root@host01 "tar -zxvf /root/installation/istio-0.6.0-linux.tar.gz -C /root/installation"
 
-ssh root@host01 "sleep 10; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
+ssh root@host01 "sleep 20; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
