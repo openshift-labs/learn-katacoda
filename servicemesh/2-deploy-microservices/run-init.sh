3ec867e1ec5a666f13f6e62ca2d90b4bb313b309
Rafael Benevides <benevides@redhat.com>
2018-06-26 20:17:16 +0200

    Several improvements for Servicemesh scenarios (#221)
    * Added a finish chapter to all scenarios
    
    * Fix #220 - Replaced script to wait for OpenShift
    
    * Get Istio-tutorial from a Katacoda branch
    
    * Remove /root/projects/incubator-openwhisk-devtools
    
    * Return Rate Limiting use case

--- servicemesh/2-deploy-microservices/run-init.sh
+++ servicemesh/2-deploy-microservices/run-init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml"
+ssh root@host01 "until (oc status &> /dev/null); do sleep 1; done"
+
+ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml /root/projects/incubator-openwhisk-devtools"
 
 ssh root@host01 "wget -c https://github.com/istio/istio/releases/download/0.6.0/istio-0.6.0-linux.tar.gz -P /root/installation"
 
 ssh root@host01 "tar -zxvf /root/installation/istio-0.6.0-linux.tar.gz -C /root/installation"
 
-ssh root@host01 "sleep 20; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
+ssh root@host01 "oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
--- servicemesh/3-monitoring-tracing/run-init.sh
+++ servicemesh/3-monitoring-tracing/run-init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+ssh root@host01 "until (oc status &> /dev/null); do sleep 1; done"
+
 ssh root@host01 "wget -c https://github.com/istio/istio/releases/download/0.6.0/istio-0.6.0-linux.tar.gz -P /root/installation"
 
-ssh root@host01 "git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git pull"
-ssh root@host01 "rm -rf /root/projects/rhoar-getting-started /root/temp-pom.xml"
+ssh root@host01 "git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git fetch"
+ssh root@host01 "git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git checkout katacoda"
+ssh root@host01 "rm -rf /root/projects/rhoar-getting-started /root/temp-pom.xml /root/projects/incubator-openwhisk-devtools"
 
 ssh root@host01 "tar -zxvf /root/installation/istio-0.6.0-linux.tar.gz -C /root/installation"
 
-ssh root@host01 "sleep 20; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
+ssh root@host01 "oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
