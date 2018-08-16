7fd7d1d8be805cc032b19354e2b1af389d658a53
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-16 13:39:23 +1000

    Split loading of templates into separate script and log steps.

---
+++ playgrounds/openshift39/assets/setup-templates.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+{
+set -x
+
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/mariadb-ephemeral-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/mariadb-persistent-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/mongodb-ephemeral-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/mongodb-persistent-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/mysql-ephemeral-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/mysql-persistent-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/postgresql-ephemeral-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/postgresql-persistent-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/redis-ephemeral-template.json -n openshift
+    oc create -f https://github.com/openshift/origin/raw/release-3.9/examples/db-templates/redis-persistent-template.json -n openshift
+
+} > /tmp/setup-volumes.log 2>&1
