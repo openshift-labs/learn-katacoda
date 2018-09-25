fcff2f8e81830c48bc3c4b4f35bc28e39bc6ea7f
Rafael Benevides <rafabene@gmail.com>
2018-09-25 10:54:21 -0400

    Ignore errors on Makefile

--- servicemesh/2-deploy-microservices/run-init.sh
+++ servicemesh/2-deploy-microservices/run-init.sh
@@ -4,10 +4,9 @@ rm -rf /root/temp-pom.xml /root/projects/*
 #TEMPORARY FIX for this image
 hostname -I | tr ' ' '\n' | awk NF | awk '{print $1 " master"}' | tee -a /etc/hosts ; systemctl restart dnsmasq ; setenforce 0
 
-
 until (oc status &> /dev/null); do sleep 1; done
 
 git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git fetch
 git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git checkout katacoda
-make -f /root/projects/istio-tutorial/Makefile cleanup istio
+make -i -f /root/projects/istio-tutorial/Makefile cleanup istio
 rm -fR /root/projects/istio-tutorial
