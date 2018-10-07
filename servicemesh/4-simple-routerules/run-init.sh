d693ed5dd3587d9dca2bbfa77d648f259828409e
Ben Hall <Ben@Benhall.me.uk>
2018-10-08 00:03:07 +0100

    Update run-init.sh

--- servicemesh/4-simple-routerules/run-init.sh
+++ servicemesh/4-simple-routerules/run-init.sh
@@ -9,4 +9,4 @@ git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tu
 git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git checkout katacoda
 
 until (oc status &> /dev/null); do sleep 1; done
-make -i -f /root/projects/istio-tutorial/Makefile cleanup istioprojects/istio-tutorial/Makefile
+make -i -f /root/projects/istio-tutorial/Makefile cleanup istio
