c268679dacfab78015d35f0d4f2bc291c26b3af3
Rafael Benevides <rafabene@gmail.com>
2018-10-08 13:58:17 -0400

    Fix makefile for istio scenarios

--- servicemesh/2-deploy-microservices/run-init.sh
+++ servicemesh/2-deploy-microservices/run-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf /root/temp-pom.xml /root/projects/*
 
 #TEMPORARY FIX for this image
 hostname -I | tr ' ' '\n' | awk NF | awk '{print $1 " master"}' | tee -a /etc/hosts ; systemctl restart dnsmasq ; setenforce 0
@@ -10,4 +9,3 @@ git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tu
 
 until (oc status &> /dev/null); do sleep 1; done
 make -i -f /root/projects/istio-tutorial/Makefile cleanup istio
-rm -fR /root/projects/istio-tutorial
