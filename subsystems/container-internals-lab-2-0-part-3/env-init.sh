ba4565653af9a91f616ce13548aa45d83b78b57c
Scott McCarty <smccarty@redhat.com>
2018-10-10 16:51:05 -0400

    SM: updated env-init

--- subsystems/container-internals-lab-2-0-part-3/env-init.sh
+++ subsystems/container-internals-lab-2-0-part-3/env-init.sh
@@ -1,6 +1,6 @@
 # General Preparation
 ssh root@host01 'git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs'
-ssh root@host01 'yum install -y podman buildah skopeo-containers tree httpd-tools strace &'
+ssh root@host01 'yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace &'
 
 # Lab 1
 ssh root@host01 'cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh'
