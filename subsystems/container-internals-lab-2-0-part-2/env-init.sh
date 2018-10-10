f8d6453bb9210568dcbc4b5c5d4ec29eb710a78d
Scott McCarty <smccarty@redhat.com>
2018-10-10 14:53:31 -0400

    SM: updated env-init

--- subsystems/container-internals-lab-2-0-part-2/env-init.sh
+++ subsystems/container-internals-lab-2-0-part-2/env-init.sh
@@ -1,27 +1,25 @@
 # General Preparation
 ssh root@host01 'git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs'
+ssh root@host01 'yum install -y podman buildah skopeo-containers tree httpd-tools strace &'
 
 # Lab 1
 ssh root@host01 'cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh'
 
 # Lab 2
-ssh root@host01 'yum install -y podman buildah skopeo-containers'
 
 # Lab 3
-ssh root@host01 'rpm -ivh http://mirror.centos.org/centos/7/os/x86_64/Packages/tree-1.6.0-10.el7.x86_64.rpm'
+ssh root@host01 'yum install '
 
 # Lab 4
 ssh root@host01 'cp ~/labs/lab3-step1/atomic-openshift-master.service /etc/systemd/system/atomic-openshift-master.service'
 ssh root@host01 'cp ~/labs/lab3-step1/atomic-openshift-node.service /etc/systemd/system/atomic-openshift-node.service'
 ssh root@host01 'systemctl disable --now origin.service'
 ssh root@host01 'systemctl enable --now atomic-openshift-master.service; systemctl enable --now atomic-openshift-node.service'
-ssh root@host01 'rpm -ivh http://mirror.centos.org/centos/7/os/x86_64/Packages/strace-4.12-6.el7.x86_64.rpm'
 
 # Lab 5
 ssh root@host01 'git clone --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/lab4-step1'
 ssh root@host01 '~/labs/lab4-step1/create.sh'
 ssh root@host01 'sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/lab4-step1/wordpress-objects.yaml'
-ssh root@host01 'rpm -ivh http://mirror.centos.org/centos/7/os/x86_64/Packages/httpd-tools-2.4.6-80.el7.centos.x86_64.rpm http://mirror.centos.org/centos/7/os/x86_64/Packages/apr-1.4.8-3.el7_4.1.x86_64.rpm http://mirror.centos.org/centos/7/os/x86_64/Packages/apr-util-1.5.2-6.el7.x86_64.rpm'
 
 # Lab 8
 ssh root@host01 'git clone --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/'
