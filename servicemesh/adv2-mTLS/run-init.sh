3225f0192a76d6530b61e00f48205a62b7d1ce1a
Rafael Benevides <rafabene@gmail.com>
2018-11-06 20:43:59 -0200

    Add mTLS scenario

---
+++ servicemesh/adv2-mTLS/run-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#TEMPORARY FIX for this image
+hostname -I | tr ' ' '\n' | awk NF | awk '{print $1 " master"}' | tee -a /etc/hosts ; systemctl restart dnsmasq ; setenforce 0
+
+git clone https://github.com/redhat-developer-demos/istio-tutorial/ /root/projects/istio-tutorial/ || true
+git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git fetch
+git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git checkout katacoda
+
+until (oc status &> /dev/null); do sleep 1; done
+make -i -f /root/projects/istio-tutorial/Makefile cleanup istio new-project microservices
