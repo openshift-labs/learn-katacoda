efa945d41bf6e024e43a4d508cc628ab7cef58ad
jamesfalkner <schtool@gmail.com>
2018-11-13 12:57:39 -0500

    workaround centos cert issue

--- middleware/rhoar-getting-started-nodejs/run-init.sh
+++ middleware/rhoar-getting-started-nodejs/run-init.sh
@@ -1,2 +1,3 @@
 ssh root@host01 "curl -k --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -"
 ssh root@host01 "yum -y install nodejs npm"
+ssh root@host01 "touch /etc/rhsm/ca/redhat-uep.pem"
