1efca77b04a135737f2174b2a7f31a8011e3a8db
jamesfalkner <schtool@gmail.com>
2018-11-13 13:44:16 -0500

    wait for istio tar file to download

--- servicemesh/1-introduction/set-env.sh
+++ servicemesh/1-introduction/set-env.sh
@@ -1,4 +1,4 @@
-"Downloading Istio..."
+echo "Downloading Istio..."
 until $(tar -tvf /root/installation/istio-1.0.2-linux.tar.gz &> /dev/null); do (echo -n .; sleep 2); done;
 /usr/local/bin/launch.sh
 mkdir -p /root/installation && cd ~/installation
