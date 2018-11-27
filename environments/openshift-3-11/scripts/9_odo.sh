a726cb35f50b7d72e2cf8cd8447edd17cc2d5a78
Charles Moulliard <cmoulliard@redhat.com>
2018-11-27 13:28:34 +0100

    Add command to download and install odo

--- environments/openshift-3-11/scripts/9_odo.sh
+++ environments/openshift-3-11/scripts/9_odo.sh
@@ -1,1 +1,6 @@
-echo "**** Configuring ODO etc ****"
+#!/bin/bash
+
+echo "**** Installing ODO using latest release ****"
+curl -L https://github.com/redhat-developer/odo/raw/master/scripts/install.sh | bash
+
+
