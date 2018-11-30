672b7a87427e04ae22d12346a42a6b1a8ee27919
Ben Hall <ben@benhall.me.uk>
2018-11-30 21:28:30 +0000

    Fix ENV setting

--- playgrounds/openshift311-cluster/set-env.sh
+++ playgrounds/openshift311-cluster/set-env.sh
@@ -1,2 +1,2 @@
-echo "echo 'export HOST1_IP=[[HOST_IP]]' >> ~/.env; echo 'export HOST2_IP=[[HOST2_IP]]' >> ~/.env"; source ~/.env
+echo "export HOST1_IP=[[HOST_IP]]; export HOST2_IP=[[HOST2_IP]]" >> ~/.env; source ~/.env
 launch.sh
