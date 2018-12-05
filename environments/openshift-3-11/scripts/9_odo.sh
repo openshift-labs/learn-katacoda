f473cf3ab4db423f9ddef5c8ac32c0b9b641e9d2
jankleinert <jan.kleinert@redhat.com>
2018-12-05 12:33:31 -0500

    Updating odo to version 0.0.17

--- environments/openshift-3-11/scripts/9_odo.sh
+++ environments/openshift-3-11/scripts/9_odo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "**** Installing ODO using binary file ****"
-sudo curl -L https://github.com/redhat-developer/odo/releases/download/v0.0.16/odo-linux-amd64 -o /usr/local/bin/odo && sudo chmod +x /usr/local/bin/odo
+sudo curl -L https://github.com/redhat-developer/odo/releases/download/v0.0.17/odo-linux-amd64 -o /usr/local/bin/odo && sudo chmod +x /usr/local/bin/odo
 
 
