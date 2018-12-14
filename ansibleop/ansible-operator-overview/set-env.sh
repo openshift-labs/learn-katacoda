73abda7474a9aa81356b3594f5056727db47dc28
Matt Dorn <madorn@gmail.com>
2018-12-14 10:40:35 -0600

    temporary set-env script fix for workshop (#303)
    temporary set-env script fix for workshop
    
    tsanders has already made these changes in the base image, so these should be removed after the workshop.

--- ansibleop/ansible-operator-overview/set-env.sh
+++ ansibleop/ansible-operator-overview/set-env.sh
@@ -1,2 +1,6 @@
+export GOPATH=/root/tutorial/go
+export GOBIN=/root/tutorial/go/bin
+wget https://github.com/operator-framework/operator-sdk/releases/download/v0.3.0/operator-sdk-v0.3.0-x86_64-linux-gnu -O /root/tutorial/go/bin/operator-sdk
+chmod +x /root/tutorial/go/bin/operator-sdk
 #switch user to tutorial directory
 cd ~/tutorial
