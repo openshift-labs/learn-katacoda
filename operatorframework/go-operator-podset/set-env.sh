e6fce60ae6905e9c159a9b9400499f1bcc97c77e
Matt Dorn <madorn@gmail.com>
2018-12-13 23:35:37 -0600

    get operator-sdk v0.2.1 and set GOBIN

--- operatorframework/go-operator-podset/set-env.sh
+++ operatorframework/go-operator-podset/set-env.sh
@@ -1,2 +1,5 @@
+wget https://github.com/operator-framework/operator-sdk/releases/download/v0.2.1/operator-sdk-v0.2.1-x86_64-linux-gnu -O /root/tutorial/go/bin/operator-sdk
+chmod +x /root/tutorial/go/bin/operator-sdk
+export GOBIN=/root/tutorial/go/bin
 #switch user to tutorial directory
 cd ~/tutorial
