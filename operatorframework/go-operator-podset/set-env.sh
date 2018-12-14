4fd2f530e294472875af7d62de44aadfbb57b48a
Matt Dorn <madorn@gmail.com>
2018-12-13 19:23:36 -0600

    remove setenv lines for operator-sdk

--- operatorframework/go-operator-podset/set-env.sh
+++ operatorframework/go-operator-podset/set-env.sh
@@ -1,8 +1,2 @@
 #switch user to tutorial directory
-export GOROOT=/usr/local/go
-export PATH=$PATH:$GOROOT/bin
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-mv /root/tutorial/go/src/github.com/operator-framework/operator-sdk /root/tutorial/go/bin/operator-sdk
 cd ~/tutorial
-#sleep 20
-#source /root/.bashrc
