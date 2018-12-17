067d833e2174249c2aa283c4a4dd1682b82ba0eb
Todd Sanders <tsanders@redhat.com>
2018-12-17 08:55:45 -0500

    Updating operator-sdk installation to v0.3.0 and fixing ansible-runner install. (#302)

--- environments/openshift-3-11/scripts/10_op-sdk-setup.sh
+++ environments/openshift-3-11/scripts/10_op-sdk-setup.sh
@@ -9,15 +9,16 @@ pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trus
 
 echo "install ansible runner -> this will be used for operator-sdk local installs"
 yum install python-devel -y
-pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org ansible-runner
-pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org ansible-runner-http
+yum remove python-requests -y
+pip uninstall requests -y
+pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org requests ansible-runner ansible-runner-http idna==2.7
 
 echo "setup GoLang Environment"
 wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz -P /tmp
 tar -C /usr/local -xzf /tmp/go1.10.linux-amd64.tar.gz
-echo "export GOPATH=$HOME/tutorial/go" >> ~/.bashrc
+echo "export GOPATH=\$HOME/tutorial/go" >> ~/.bashrc
 echo "export GOROOT=/usr/local/go" >> ~/.bashrc
-echo "export GOBIN=$GOPATH/bin" >> ~/.bashrc
+echo "export GOBIN=\$GOPATH/bin" >> ~/.bashrc
 echo "export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin" >> ~/.bashrc
 . ~/.bashrc
 
@@ -28,9 +29,8 @@ curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 mkdir -p $GOPATH/{src,pkg,bin}
 
 echo "install Operator sdk"
-mkdir -p $GOPATH/bin
 cd $GOPATH/bin
-wget https://github.com/operator-framework/operator-sdk/releases/download/v0.2.1/operator-sdk-v0.2.1-x86_64-linux-gnu -O operator-sdk
+wget https://github.com/operator-framework/operator-sdk/releases/download/v0.3.0/operator-sdk-v0.3.0-x86_64-linux-gnu -O operator-sdk
 chmod +x operator-sdk
 git config --global user.email "operator-sdk@example.com"
 git config --global user.name "OperatorSDK Katacoda"
