f33ac63d544e0161e9dca1881c92f0f6432e9d37
Matt Dorn <madorn@gmail.com>
2019-01-17 17:29:04 -0600

    temporarily move dep install to set-env.sh
    temporarily move dep install to set-env.sh

--- operatorframework/go-operator-podset/set-env.sh
+++ operatorframework/go-operator-podset/set-env.sh
@@ -1,5 +1,6 @@
 wget https://github.com/operator-framework/operator-sdk/releases/download/v0.2.1/operator-sdk-v0.2.1-x86_64-linux-gnu -O /root/tutorial/go/bin/operator-sdk
 chmod +x /root/tutorial/go/bin/operator-sdk
 export GOBIN=/root/tutorial/go/bin
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 #switch user to tutorial directory
 cd ~/tutorial
