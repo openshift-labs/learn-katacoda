105aba8a5de8d60b4cacd5e339d18cc4230c0eef
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-02-15 11:04:33 +0100

    Upgraded script to work with OpenShift 3.11.

--- middleware/dm7-dmn-introduction/set-env.sh
+++ middleware/dm7-dmn-introduction/set-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-until [ -f /openshift.local.config/master/master-config.yaml ] ; do echo -n . && sleep 5; done
-sed -i '213s/\.environments\.katacoda\.com/.http-proxy.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+until [ -f /etc/origin/master/master-config.yaml ] ; do echo -n . && sleep 5; done
+sed -i '181s/\.environments\.katacoda\.com/.http-proxy.katacoda.com/' /etc/origin/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
 ~/.init/init-scenario.sh
