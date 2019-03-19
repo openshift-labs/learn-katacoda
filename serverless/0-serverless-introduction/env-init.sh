edd87ada4e85606426ec2b95bfab97551a79349b
Jim Curtis <jim.curtis@redhat.com>
2019-03-19 11:31:17 -0700

    Delete env-init.sh

--- serverless/0-serverless-introduction/env-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ssh root@host01 "rm -rf /root/projects"
-ssh root@host01 "mkdir -p /root/projects/ocf && mkdir -p /root/openwhisk/bin/"
-ssh root@host01 "mkdir -p ~/root/projects/getting-started"
