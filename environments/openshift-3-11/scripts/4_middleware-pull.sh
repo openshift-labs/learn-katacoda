ced6004e6f0bd3f4b384c4cf8d90d40b61132589
Rafael Benevides <rafabene@gmail.com>
2019-03-26 12:15:09 -0400

    Update Openshift to 3.11 and Istio to 1.0.5

--- environments/openshift-3-11/scripts/4_middleware-pull.sh
+++ environments/openshift-3-11/scripts/4_middleware-pull.sh
@@ -1,2 +1,3 @@
 docker pull fabric8/java-jboss-openjdk8-jdk:1.3.1
 docker pull fabric8/java-jboss-openjdk8-jdk:1.5.1
+docker pull fabric8/java-jboss-openjdk8-jdk:1.5.2
--- servicemesh/1-introduction/set-env.sh
+++ servicemesh/1-introduction/set-env.sh
@@ -1,4 +1,4 @@
 echo "Downloading Istio..."
-until $(tar -tvf /root/installation/istio-1.0.2-linux.tar.gz &> /dev/null); do (echo -n .; sleep 2); done;
+until $(tar -tvf /root/installation/istio-1.0.5-linux.tar.gz &> /dev/null); do (echo -n .; sleep 2); done;
 /usr/local/bin/launch.sh
 mkdir -p /root/installation && cd ~/installation
