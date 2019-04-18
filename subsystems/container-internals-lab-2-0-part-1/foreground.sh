e1895c399f75d1785a131b3d0716617ee405c759
Scott McCarty <smccarty@redhat.com>
2019-04-18 17:43:17 -0400

    SM: playing with foreground scripts

--- subsystems/container-internals-lab-2-0-part-1/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-1/foreground.sh
@@ -1,6 +1,6 @@
 # General Preparation
-ssh root@host01 'git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs'
-ssh root@host01 'yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace &'
+git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
+yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
 
 # Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
