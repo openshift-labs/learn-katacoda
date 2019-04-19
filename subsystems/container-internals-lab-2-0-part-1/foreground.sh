4de5a6b5cdd1cfd3f21941a11e349c05ad92728c
Scott McCarty <smccarty@redhat.com>
2019-04-19 09:30:18 -0400

    SM: added image cache

--- subsystems/container-internals-lab-2-0-part-1/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-1/foreground.sh
@@ -4,4 +4,5 @@ yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
 
 # Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
+podman pull quay.io/fatherlinux/linux-container-internals-2-0-introduction
 echo "Container host is now ready."
