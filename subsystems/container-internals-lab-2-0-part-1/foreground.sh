09587e2a3207398f3eb7bd5546bac076f5c761e1
Scott McCarty <smccarty@redhat.com>
2019-04-19 13:02:41 -0400

    SM: copy images over to docker for orchestration work

--- subsystems/container-internals-lab-2-0-part-1/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-1/foreground.sh
@@ -5,4 +5,6 @@ yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
 # Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
 podman pull quay.io/fatherlinux/linux-container-internals-2-0-introduction
+skopeo copy containers-storage:registry.access.redhat.com/ubi7/ubi:latest docker-daemon:registry.access.redhat.com/ubi7/ubi:latest
+skopeo copy containers-storage:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest docker-daemon:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest
 echo "Container host is now ready."
