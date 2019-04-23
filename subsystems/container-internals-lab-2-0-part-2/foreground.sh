31f2daad3f01382615319c5ae46e809ec8b1b220
Scott McCarty <smccarty@redhat.com>
2019-04-23 19:14:15 -0400

    SM: added minimal image

--- subsystems/container-internals-lab-2-0-part-2/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-2/foreground.sh
@@ -5,3 +5,4 @@ yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace &
 # Lab 2
 yum install -y podman buildah skopeo
 podman pull registry.access.redhat.com/ubi7/ubi
+podman pull registry.access.redhat.com/ubi7/ubi-minimal
