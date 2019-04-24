14f2b9d007dbff14e0ce064bfcb984452638dc49
Scott McCarty <smccarty@redhat.com>
2019-04-23 23:16:47 -0400

    SM: fixed typo

--- subsystems/container-internals-lab-2-0-part-4/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-4/foreground.sh
@@ -7,7 +7,7 @@ podman pull registry.access.redhat.com/ubi7/ubi
 skopeo copy containers-storage:registry.access.redhat.com/ubi7/ubi:latest docker-daemon:registry.access.redhat.com/ubi7/ubi:latest
 cp ~/labs/lab3-step1/atomic-openshift-master.service /etc/systemd/system/atomic-openshift-master.service
 cp ~/labs/lab3-step1/atomic-openshift-node.service /etc/systemd/system/atomic-openshift-node.service
-systemctl disable --now origin.service'
+systemctl disable --now origin.service
 systemctl enable --now atomic-openshift-master.service; systemctl enable --now atomic-openshift-node.service
 
 # Final Preparation
