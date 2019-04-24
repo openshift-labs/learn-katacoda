c468bd868f303a060ae133a7a6760966bbffce4b
Scott McCarty <smccarty@redhat.com>
2019-04-24 17:06:20 -0400

    SM: update lab 4

--- subsystems/container-internals-lab-2-0-part-4/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-4/foreground.sh
@@ -1,6 +1,7 @@
 # General Preparation
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
 yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
+cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh
 
 # Lab 4
 podman pull registry.access.redhat.com/ubi7/ubi
