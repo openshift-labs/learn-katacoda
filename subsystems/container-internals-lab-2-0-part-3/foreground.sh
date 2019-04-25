b33e463b2be379b1119272316287c31727d98a84
Scott McCarty <smccarty@redhat.com>
2019-04-25 13:34:48 -0400

    SM: fixed foreground.sh

--- subsystems/container-internals-lab-2-0-part-3/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-3/foreground.sh
@@ -1,8 +1,9 @@
 # General Preparation
-ssh root@host01 'git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs'
-ssh root@host01 'yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace &'
-ssh root@host01 'cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh'
+git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
+yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
+cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh
 
 # Lab 3
+podman pull registry.access.redhat.com/ubi7/ubi
 
 echo "Container host is now ready."
