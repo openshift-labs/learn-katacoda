45b58fd6bd6b9a1b5de103a42a632af7daf05f67
Ben Hall <ben@benhall.me.uk>
2019-05-23 07:31:45 +0000

    Fix typo. Closes #460

--- subsystems/container-internals-lab-2-0-part-5/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-5/foreground.sh
@@ -4,7 +4,7 @@ yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
 cp ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh
 
 # Lab 5
-podman pull registry.access.redaht.com/ubi7/ubi
+podman pull registry.access.redhat.com/ubi7/ubi
 git clone --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/lab4-step1
 ~/labs/lab4-step1/create.sh
 sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/lab4-step1/wordpress-objects.yaml
