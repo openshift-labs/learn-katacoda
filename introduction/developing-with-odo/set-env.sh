8f4503b04c2156c9a30c4e7ba9cadfc0fdb9fcb7
danielhelfand <helfand.4@gmail.com>
2019-06-03 14:13:47 -0400

    removing odo v1.0.0-beta2 download

--- introduction/developing-with-odo/set-env.sh
+++ introduction/developing-with-odo/set-env.sh
@@ -28,9 +28,4 @@ git clone https://github.com/openshift-evangelists/Wild-West-Frontend.git ~/fron
 
 clear
 
-echo "Downloading odo v1.0.0-beta2"
-sudo curl -L https://github.com/redhat-developer/odo/releases/download/v1.0.0-beta2/odo-linux-amd64 -o /usr/local/bin/odo && sudo chmod +x /usr/local/bin/odo
-
-clear
-
 echo "Configuration completed"
