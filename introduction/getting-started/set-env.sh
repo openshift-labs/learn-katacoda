da50384e9f417eaf1d19a3b388c11e95f75326cf
danielhelfand <helfand.4@gmail.com>
2019-06-19 11:31:40 -0400

    address error: x509: certificate signed by unknown authority

--- introduction/getting-started/set-env.sh
+++ introduction/getting-started/set-env.sh
@@ -1,2 +1,7 @@
 #No setup required for this scenario, but set-env.sh is required by katacoda framework
+echo "Configuring scenario"
+
+curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
+oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
+
 echo "Configuration completed"
