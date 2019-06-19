3915092bb8375dd28d9c240eec7d118c52d93065
danielhelfand <helfand.4@gmail.com>
2019-06-19 11:34:44 -0400

    updating set-env.sh comment

--- introduction/getting-started/set-env.sh
+++ introduction/getting-started/set-env.sh
@@ -1,7 +1,9 @@
-#No setup required for this scenario, but set-env.sh is required by katacoda framework
 echo "Configuring scenario"
 
+#Helps to address error: x509: certificate signed by unknown authority
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
 oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
+
+clear
 
 echo "Configuration completed"
