1bfa1ec4f77e64db051ba766ace388e83dec7bf7
Ben Hall <Ben@Benhall.me.uk>
2019-07-01 11:22:29 +0100

    Update confirmation message

--- introduction/getting-started/set-env.sh
+++ introduction/getting-started/set-env.sh
@@ -1,9 +1,7 @@
-echo "Configuring scenario"
-
 #Helps to address error: x509: certificate signed by unknown authority
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
 oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
 
 clear
 
-echo "Configuration completed"
+echo -e "\e[32mWelcome to your interactive environment. OpenShift is configured and ready to use.\e[0m"
