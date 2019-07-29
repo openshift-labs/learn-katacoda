cdb2217a76d4b714e1ced15c6b0711d5fd4ae161
Scott McCarty <scott.mccarty@gmail.com>
2019-07-29 16:55:38 -0400

    SM: added new git clone command

--- subsystems/container-internals-lab-2-0-part-5/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-5/foreground.sh
@@ -8,3 +8,4 @@ podman pull registry.access.redhat.com/ubi7/ubi
 git clone --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/lab4-step1
 ~/labs/lab4-step1/create.sh
 sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/lab4-step1/wordpress-objects.yaml
+git clone --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/
