495ed00718bcb99c941b6175c3b76030de24edc9
Ben Hall <Ben@Benhall.me.uk>
2019-08-23 07:42:41 +0100

    Update 9_odo.sh

--- environments/openshift-3-11/scripts/9_odo.sh
+++ environments/openshift-3-11/scripts/9_odo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "**** Installing ODO using binary file ****"
-sudo curl -L https://github.com/redhat-developer/odo/releases/download/v1.0.0-beta2/odo-linux-amd64 -o /usr/local/bin/odo && sudo chmod +x /usr/local/bin/odo
+
+sudo curl -L https://github.com/openshift/odo/raw/master/scripts/installer.sh | sudo bash
