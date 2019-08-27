4a8823a93a5d374f6362de08485ba6781d1f362f
Ben Hall <ben@benhall.me.uk>
2019-08-27 08:18:07 +0100

    Use beta2 of odo - Fixes #550

--- environments/openshift-3-11/scripts/9_odo.sh
+++ environments/openshift-3-11/scripts/9_odo.sh
@@ -2,4 +2,5 @@
 
 echo "**** Installing ODO using binary file ****"
 
-sudo curl -L https://github.com/openshift/odo/raw/master/scripts/installer.sh | sudo bash
+curl -L https://github.com/openshift/odo/releases/download/v1.0.0-beta2/odo-linux-amd64 -o /usr/local/bin/odo && chmod +x /usr/local/bin/odo
+curl -L -o /opt/lets-encrypt-x3-cross-signed.pem https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem 
