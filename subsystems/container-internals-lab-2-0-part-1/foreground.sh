7517b3a8001f053062be654db9577a366deace9e
Scott McCarty <scott.mccarty@gmail.com>
2019-09-11 15:51:27 -0400

    SM: hack to remove currently installed version of podman

--- subsystems/container-internals-lab-2-0-part-1/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-1/foreground.sh
@@ -2,6 +2,7 @@
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
 yum install -y yum-plugin-versionloc
 yum versionlock podman-1.3.2
+yum remove podman buildah skopeo
 yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
 
 # Lab 1
