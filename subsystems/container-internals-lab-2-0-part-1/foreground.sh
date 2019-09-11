ac001f75ae3a053c14a070f94156dac0eac81bed
Scott McCarty <scott.mccarty@gmail.com>
2019-09-11 14:03:11 -0400

    SM: set specific version of podman to prevent regression

--- subsystems/container-internals-lab-2-0-part-1/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-1/foreground.sh
@@ -1,6 +1,6 @@
 # General Preparation
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
+yum install -y podman-1.3.2-1 buildah skopeo skopeo-containers tree httpd-tools strace
 
 # Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
