b5f17ab0feeb75974912621ca6d2123939e00020
jamesfalkner <schtool@gmail.com>
2019-09-17 08:51:48 -0700

    pre-pull quarkus scenario images

--- environments/openshift-3-11/scripts/4_middleware-pull.sh
+++ environments/openshift-3-11/scripts/4_middleware-pull.sh
@@ -2,3 +2,6 @@ docker pull fabric8/java-jboss-openjdk8-jdk:1.3.1
 docker pull fabric8/java-jboss-openjdk8-jdk:1.5.1
 docker pull fabric8/java-jboss-openjdk8-jdk:1.5.2
 docker pull fabric8/java-jboss-openjdk8-jdk:1.5.4
+docker pull docker.io/postgres:10.5
+docker pull registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift:1.5
+docker pull quay.io/quarkus/ubi-quarkus-native-binary-s2i:19.2.0
