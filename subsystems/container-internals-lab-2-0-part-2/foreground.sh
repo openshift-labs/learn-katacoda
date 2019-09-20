8d7e5bd3e5696db7418cf6a8818df7ee68979872
Ben Hall <ben@benhall.me.uk>
2019-09-20 14:50:33 +0100

    Install Podman 1.3.2 rpm direct as removed from mirror. Fixes #567

--- subsystems/container-internals-lab-2-0-part-2/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-2/foreground.sh
@@ -1,12 +1,11 @@
 # General Preparation
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install -y yum-plugin-versionlock
-yum versionlock podman-1.3.2
+
 yum remove -y podman
-yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
+yum install -y http://assets.joinscrapbook.com/redhat/packages/podman-1.3.2-1.git14fdcd0.el7.centos.x86_64.rpm
+yum install -y buildah skopeo skopeo-containers tree httpd-tools strace
 
 # Lab 2
-yum install -y podman buildah skopeo
 podman pull registry.access.redhat.com/ubi7/ubi
 podman pull registry.access.redhat.com/ubi7/ubi-minimal
 
--- subsystems/container-internals-lab-2-0-part-5/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-5/foreground.sh
@@ -1,9 +1,10 @@
 # General Preparation
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install -y yum-plugin-versionlock
-yum versionlock podman-1.3.2
+
 yum remove -y podman
-yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
+yum install -y http://assets.joinscrapbook.com/redhat/packages/podman-1.3.2-1.git14fdcd0.el7.centos.x86_64.rpm
+yum install -y buildah skopeo skopeo-containers tree httpd-tools strace
+
 cp -f ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh
 
 # Lab 5
