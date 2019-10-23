1280ba80e8fe2fd579af14b84cf8445cce6d7352
Jiri Pechanec <jpechane@redhat.com>
2019-10-23 05:47:12 +0200

    Upgrade to Debezium 0.10 and Strimzi 0.14

--- middleware/debezium-getting-started/assets/setup-openshift.sh
+++ middleware/debezium-getting-started/assets/setup-openshift.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/bash
-DEBEZIUM_TAG=0.7
-STRIMZI_VERSION=0.2.0
+DEBEZIUM_TAG=0.10
+STRIMZI_VERSION=0.14.0
 
 # Get Strimzi
-git clone -b $STRIMZI_VERSION https://github.com/strimzi/strimzi
-cd strimzi
+curl -kL https://github.com/strimzi/strimzi-kafka-operator/releases/download/$STRIMZI_VERSION/strimzi-$STRIMZI_VERSION.tar.gz | tar xz
+cd strimzi-$STRIMZI_VERSION
 
 # Create project
 oc new-project debezium
 
 # Install controller and templates
-oc create -f examples/install/cluster-controller && oc create -f examples/templates/cluster-controller
+sed -i 's/namespace: .*/namespace: debezium/' install/cluster-operator/*RoleBinding*.yaml
+kubectl apply -f install/cluster-operator -n debezium && oc create -f examples/templates/cluster-operator
 
 # Start pre-populated database
 oc new-app --name=mysql debezium/example-mysql:${DEBEZIUM_TAG}
-oc env dc/mysql MYSQL_ROOT_PASSWORD=debezium  MYSQL_USER=mysqluser MYSQL_PASSWORD=mysqlpw
+oc set env dc/mysql MYSQL_ROOT_PASSWORD=debezium  MYSQL_USER=mysqluser MYSQL_PASSWORD=mysqlpw
 
 cd ~/debezium
