bb7df4b8c5463b642694d57509b56c60d53c6e96
Scott McCarty <scott.mccarty@gmail.com>
2019-10-29 13:22:49 -0400

    SM: updated to new foreground/background methodology

--- subsystems/container-internals-lab-2-0-part-6/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-6/foreground.sh
@@ -1,19 +1,17 @@
 # General Preparation
+# General Preparation
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
 yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
 yum install -y https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/noarch/podman-manpages-1.5.1-3.el7.noarch.rpm \
 https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/x86_64/podman-1.5.1-3.el7.x86_64.rpm \
 https://cbs.centos.org/kojifiles/packages/slirp4netns/0.3.0/2.git4992082.el7/x86_64/slirp4netns-0.3.0-2.git4992082.el7.x86_64.rpm \
 https://cbs.centos.org/kojifiles/packages/libseccomp/2.4.1/0.el7/x86_64/libseccomp-2.4.1-0.el7.x86_64.rpm
-cp -f ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh
 
-# Lab 5
+# Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
-git clone --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/lab4-step1
-~/labs/lab4-step1/create.sh
-sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/lab4-step1/wordpress-objects.yaml
-git clone --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/lab2-step4/
-systemctl restart iptables
-/var/lib/openshift/openshift admin policy add-cluster-role-to-user cluster-admin admin
-
+podman pull quay.io/fatherlinux/linux-container-internals-2-0-introduction
+skopeo copy containers-storage:registry.access.redhat.com/ubi7/ubi:latest docker-daemon:registry.access.redhat.com/ubi7/ubi:latest
+skopeo copy containers-storage:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest docker-daemon:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest
 echo "Container host is now ready."
+
+/var/lib/openshift/openshift admin policy add-cluster-role-to-user cluster-admin admin
