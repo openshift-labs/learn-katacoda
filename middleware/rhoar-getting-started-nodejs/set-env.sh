a46590894dbdf43cb3d6cc14f776cb007ed710c4
James Falkner <schtool@gmail.com>
2019-10-29 12:13:19 -0400

    middleware: Update Node.js scenario to latest node + OCP (#596)

--- middleware/rhoar-getting-started-nodejs/set-env.sh
+++ middleware/rhoar-getting-started-nodejs/set-env.sh
@@ -1,4 +1,4 @@
 echo "Installing Node.js and NPM..."
 for i in {1..200}; do npm help >& /dev/null && break || (echo -n . ; sleep 2); done
-~/.launch.sh
 cd ~/projects/rhoar-getting-started/nodejs
+clear
