0dd9c830029270edb96f45c1f3e397f45c76a6ca
Matt Dorn <madorn@gmail.com>
2019-11-11 15:47:39 -0600

    Update env-init.sh

--- operatorframework/k8s-api-fundamentals-42/env-init.sh
+++ operatorframework/k8s-api-fundamentals-42/env-init.sh
@@ -1,15 +1,2 @@
-# grant system:anonymous user the system:image-puller role
-for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done
-
-# grant system:authenticated group the sudoer cluster role
-oc adm policy add-cluster-role-to-group sudoer system:authenticated
-
-# grant developer user permissions to push images to the internal registry
-oc policy add-role-to-user registry-editor developer --as system:admin
-
-# save the existing system:admin .kube/config for up local
-mkdir -p ~/backup/.kube
-cp -r ~/.kube/config ~/backup/.kube/
-
 # install jq
 yum install jq -y
