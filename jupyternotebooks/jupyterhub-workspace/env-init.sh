8993d4bf0f559ec72134bdbc9dfaac132c2c4bb4
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-11-17 11:43:10 +1100

    Initial files on deploying JupyterHub workspace.

---
+++ jupyternotebooks/jupyterhub-workspace/env-init.sh
@@ -0,0 +1,10 @@
+oc login -u developer -p developer
+oc new-project myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.5.0/image-streams/s2i-minimal-notebook.json | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/3.4.0/image-streams/jupyterhub.json | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.4.0/templates/notebook-builder.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.4.0/templates/notebook-builder.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/3.4.0/templates/jupyterhub-builder.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/3.4.0/templates/jupyterhub-deployer.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/3.4.0/templates/jupyterhub-quickstart.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/3.4.0/templates/jupyterhub-workspace.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
---
+++ jupyternotebooks/jupyterhub-workspace/finish.md
@@ -0,0 +1,15 @@
+In this workshop you learnt how you can provide persistent workspaces for multiple users, for working on Jupyter notebooks, using JupyterHub. The environment gated access to the Jupyter notebooks using OpenShift cluster authentication. The Jupyter notebook instances were attached to the OpenShift cluster so that users could interact with and deploy workloads to the cluster required by the Jupyter notebooks.
+
+Further details on the example JupyterHub images and templates used in this workshop can be found at:
+
+* https://github.com/jupyter-on-openshift/jupyterhub-quickstart
+
+This Git repository is part of the [Jupyter on OpenShift](https://github.com/jupyter-on-openshift) project, a community project for demonstrating how Jupyter notebooks and JupyterHub can be deployed to OpenShift.
+
+Check out the other workshops here for details of how to deploy a single Jupyter notebook and create your own custom notebook images.
+
+For information on work done by Red Hat relevant to the data science, AI and ML space, visit:
+
+* https://www.openshift.com/learn/topics/ai-ml
+
+Also check out the Red Hat projects [radanalytics.io](https://radanalytics.io/) and [Open Data Hub](https://opendatahub.io/).
