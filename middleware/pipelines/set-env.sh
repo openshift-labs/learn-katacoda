9b8ee28b968d9bd83f7c5923e10b2f2bddd59d92
Joel Lord <joelphy@gmail.com>
2020-01-28 13:36:57 -0500

    Install Tekton CLI to the env

--- middleware/pipelines/set-env.sh
+++ middleware/pipelines/set-env.sh
@@ -3,6 +3,11 @@ launch.sh
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
 oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
 
-clear
+# Get the tar.xz for tekton CLI
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.6.0/tkn_0.6.0_Darwin_x86_64.tar.gz
+# Extract tkn to your PATH (e.g. /usr/local/bin)
+sudo tar xvzf tkn_0.6.0_Darwin_x86_64.tar.gz -C /usr/local/bin tkn
+
+#clear
 
 echo -e "Welcome to your interactive environment. OpenShift is configured and ready to use."
