2cc604165b8e8893c84892dd166b91a9a42cd7ca
Brian Tannous <brian.tannous@gmail.com>
2020-02-10 18:30:41 -0600

    Serverless env-init

--- introduction/serverless/env-init.sh
+++ introduction/serverless/env-init.sh
@@ -1,3 +1,4 @@
+ssh root@host01 'oc apply -f https://raw.githubusercontent.com/btannous/learn-katacoda/serverless/introduction/serverless/assets/operator-subscription.yaml'
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
 ssh root@host01 'oc adm policy add-cluster-role-to-group sudoer system:authenticated'
 
