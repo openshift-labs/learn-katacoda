d9f05682168a0da76a8e48090d8bee4cc3d040bb
Brian <b@cent.ack>
2020-02-12 17:11:52 -0600

    serving and eventing operators, instances, and samples to test

--- introduction/serverless/env-init.sh
+++ introduction/serverless/env-init.sh
@@ -1,4 +1,7 @@
 ssh root@host01 'oc apply -f https://raw.githubusercontent.com/btannous/learn-katacoda/serverless/introduction/serverless/assets/operator-subscription.yaml'
+# TODO might need to wait before running these below for the operators to come up?
+ssh root@host01 'oc apply -f https://raw.githubusercontent.com/btannous/learn-katacoda/serverless/introduction/serverless/assets/serving.yaml'
+ssh root@host01 '0c apply -f https://raw.githubusercontent.com/btannous/learn-katacoda/serverless/introduction/serverless/assets/eventing.yaml'
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
 ssh root@host01 'oc adm policy add-cluster-role-to-group sudoer system:authenticated'
 
