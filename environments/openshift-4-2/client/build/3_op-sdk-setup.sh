05f130117d015830ba4d4f06fdb9cfb0ed5b5ebf
Matt Dorn <madorn@gmail.com>
2020-03-06 15:39:32 -0600

    update operator-sdk build script for openshift-4.2 image (#670)
    * Update 3_op-sdk-setup.sh
    
    * Update 3_op-sdk-setup.sh

--- environments/openshift-4-2/client/build/3_op-sdk-setup.sh
+++ environments/openshift-4-2/client/build/3_op-sdk-setup.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
+echo "install gcc"
+yum install gcc -y
+
 echo "install openshift client"
 yum --enablerepo=extras install epel-release -y
-yum install jq -y
-yum install python3 -y
-pip3 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --upgrade pip
-pip3 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --upgrade setuptools
-pip3 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --ignore-installed ipaddress openshift
+yum install python2-pip -y
+pip2 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --upgrade pip
+pip2 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --upgrade setuptools
+pip2 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org --ignore-installed ipaddress openshift
 
 echo "install ansible runner -> this will be used for operator-sdk local installs"
-yum install gcc -y
-yum install python3-devel -y
-yum remove python3-requests -y
-pip3 uninstall requests -y
-pip3 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org requests ansible-runner ansible-runner-http idna==2.7
+yum install python2-devel -y
+pip2 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org ansible-runner
+pip2 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org ansible-runner-http
+
+echo "create tutorial dir"
+mkdir -p $HOME/tutorial
 
 echo "setup GoLang Environment"
-wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz -P /tmp
-tar -C /usr/local -xzf /tmp/go1.10.linux-amd64.tar.gz
-echo "export GOPATH=\$HOME/tutorial/go" >> ~/.bashrc
+wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz -P /tmp
+tar -C /usr/local -xzf /tmp/go1.13.1.linux-amd64.tar.gz
+echo "export GOPATH=$HOME/tutorial/go" >> ~/.bashrc
 echo "export GOROOT=/usr/local/go" >> ~/.bashrc
-echo "export GOBIN=\$GOPATH/bin" >> ~/.bashrc
+echo "export GOBIN=$GOPATH/bin" >> ~/.bashrc
 echo "export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin" >> ~/.bashrc
+echo "export GO111MODULE=on" >> ~/.bashrc
 . ~/.bashrc
 
-mkdir -p $GOBIN
 echo "install dep"
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
-#ensure needed dirs exists at GOPATH
+echo "ensure needed dirs exists at GOPATH"
 mkdir -p $GOPATH/{src,pkg,bin}
 
 echo "install Operator sdk"
-cd $GOPATH/bin
-wget https://github.com/operator-framework/operator-sdk/releases/download/v0.6.0/operator-sdk-v0.6.0-x86_64-linux-gnu -O operator-sdk
-chmod +x operator-sdk
-git config --global user.email "operator-sdk@example.com"
-git config --global user.name "OperatorSDK Katacoda"
+wget https://github.com/operator-framework/operator-sdk/releases/download/v0.15.2/operator-sdk-v0.15.2-x86_64-linux-gnu 
+chmod +x operator-sdk-v0.15.2-x86_64-linux-gnu 
+mv operator-sdk-v0.15.2-x86_64-linux-gnu /root/tutorial/go/bin/operator-sdk
