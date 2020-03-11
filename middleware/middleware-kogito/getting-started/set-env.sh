7f384bf5956bf023101b281eb125d1fd0ec7c26a
Duncan Doyle <Duncan.Doyle@gmail.com>
2020-03-11 11:34:24 +0100

    Upgraded to Quarkus 1.3.0.CR2. Removed GraalVM config from set-env.

--- middleware/middleware-kogito/getting-started/set-env.sh
+++ middleware/middleware-kogito/getting-started/set-env.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-export GRAALVM_HOME=/usr/local/graalvm-ce-19.2.1
-echo 'export GRAALVM_HOME=/usr/local/graalvm-ce-19.2.1' >> ~/.bashrc
 mkdir -p /root/projects/kogito
 cd /root/projects/kogito
 clear
