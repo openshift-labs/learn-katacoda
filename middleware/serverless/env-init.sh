65d0632e21f3b28296ce29a1255f9dc042c5f963
Brian Tannous <brian.tannous@gmail.com>
2020-03-23 17:59:50 -0500

    change order of env-init

--- middleware/serverless/env-init.sh
+++ middleware/serverless/env-init.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -o pipefail 
 
-mkdir -p /root/projects && cd /root/projects/
-
-git clone https://github.com/redhat-developer-demos/knative-tutorial
-
-cd  knative-tutorial && rm -rf !("basics"|"scaling")
-
 oc create -f https://raw.githubusercontent.com/btannous/learn-katacoda/serverless/middleware/serverless/assets/operator-subscription.yaml
 
 sleep 10
 source assets/approve-csv.bash
 approve_csv 1.4.1
+
+mkdir -p /root/projects && cd /root/projects/
+
+git clone https://github.com/redhat-developer-demos/knative-tutorial
+
+cd  knative-tutorial && rm -rf !("basics"|"scaling")
 
 oc adm new-project knativetutorial
 oc adm policy add-role-to-user admin developer -n knativetutorial
