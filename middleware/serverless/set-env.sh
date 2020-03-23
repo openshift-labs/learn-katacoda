28aeedde60460a933c7885796c817a85c876486b
Brian Tannous <brian.tannous@gmail.com>
2020-03-23 18:14:41 -0500

    is this hooking?

--- middleware/serverless/set-env.sh
+++ middleware/serverless/set-env.sh
@@ -2,4 +2,4 @@
 
 cd projects/knative-tutorial
 
-sleep 150; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 20; done
+sleep 151; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 20; done
