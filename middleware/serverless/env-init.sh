452ecfa319268d968158cf274f0c7951860bdebe
Brian Tannous <brian@briantannous.com>
2020-03-24 17:53:38 -0500

    using set-env instead of env-init for debugging

--- middleware/serverless/env-init.sh
+++ middleware/serverless/env-init.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
 set -o pipefail 
-
-oc create -f https://raw.githubusercontent.com/btannous/learn-katacoda/serverless/middleware/serverless/assets/operator-subscription.yaml
-source assets/approve-csv.bash
-while [ -z $(find_install_plan 1.4.1) ]; do sleep 10; done
-approve_csv 1.4.1
-
-mkdir -p /root/projects && cd /root/projects/
-
-git clone https://github.com/redhat-developer-demos/knative-tutorial
-
-cd  knative-tutorial && rm -rf !("basics"|"scaling")
-
-oc adm new-project knativetutorial
-oc adm policy add-role-to-user admin developer -n knativetutorial
--- middleware/serverless/set-env.sh
+++ middleware/serverless/set-env.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+git clone --depth 1 -b serverless https://github.com/btannous/learn-katacoda.git .learn-katacoda
+ln -s serverless .learn-katacoda/middleware/serverless
+cd serverless
+oc create -f assets/operator-subscription.yaml
+source assets/approve-csv.bash
+sleep 5
+while [ -z $(find_install_plan 1.4.1) ]; do sleep 10; done
+approve_csv 1.4.1
 
-cd projects/knative-tutorial
+oc adm new-project serverless-tutorial
+oc adm policy add-role-to-user admin developer -n serverless-tutorial
 
-sleep 151; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 20; done
+sleep 5; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 20; done
