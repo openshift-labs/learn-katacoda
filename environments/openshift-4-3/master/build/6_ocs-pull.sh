17d4f43dfe70422d6b7be1980656f2ede238036d
Chris Blum <cblum@redhat.com>
2020-04-20 12:04:01 +0200

    Pre-Pull OCS Operatorsource
    This should make the install more reliable

--- environments/openshift-4-3/master/build/6_ocs-pull.sh
+++ environments/openshift-4-3/master/build/6_ocs-pull.sh
@@ -2,3 +2,17 @@ sudo podman pull quay.io/mulbc/ocs-operator:katacoda
 sudo podman pull rook/ceph:v1.2.4
 sudo podman pull noobaa/noobaa-operator:2.0.9
 sudo podman pull docker.io/ceph/ceph:v14.2
+
+
+cat <<EOF | oc create -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: ocs-catalogsource
+  namespace: openshift-marketplace
+spec:
+  sourceType: grpc
+  image: quay.io/mulbc/ocs-registry:katacoda
+  displayName: OpenShift Container Storage
+  publisher: Red Hat
+EOF
