d097835ce5a2db372c908eb4ddd949e54844f0e2
danieloh30 <doh@redhat.com>
2020-04-27 16:55:48 -0400

    fix openssl installation

--- middleware/middleware-quarkus/monitoring/env-init.sh
+++ middleware/middleware-quarkus/monitoring/env-init.sh
@@ -1,4 +1,5 @@
 ssh root@host01 "mkdir -p /root/projects/quarkus"
 ssh root@host01 "touch /etc/rhsm/ca/redhat-uep.pem"
-ssh root@host01 "yum -y install openssl openssl-devel"
+ssh root@host01 "yum -y install openssl openssl-pkcs11"
+ssh roor@host01 "touch /root/projects/quarkus/test.txt"
 ssh root@host01 "echo -w\ \"\n\" >> ~/.curlrc"
