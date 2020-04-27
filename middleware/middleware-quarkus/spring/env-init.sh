3cc0f1f6ba2bb4ab03236a6303b1c223d155ea12
sshaaf <sshaaf@redhat.com>
2020-04-27 03:04:22 +0200

    add libstdc++ libs

--- middleware/middleware-quarkus/spring/env-init.sh
+++ middleware/middleware-quarkus/spring/env-init.sh
@@ -1,3 +1,4 @@
 ssh root@host01 "mkdir -p /root/projects/quarkus"
 ssh root@host01 "touch /etc/rhsm/ca/redhat-uep.pem"
 ssh root@host01 "echo -w\ \"\n\" >> ~/.curlrc"
+dnf install libstdc++ libstdc++-devel -y
