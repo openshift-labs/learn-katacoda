6fab248b28b0018672d2e28a45e499ef09d30824
Duncan Doyle <Duncan.Doyle@gmail.com>
2020-05-08 09:24:51 +0200

    Changed to git clone via https.

--- middleware/middleware-kogito/rest-service-integration/set-env.sh
+++ middleware/middleware-kogito/rest-service-integration/set-env.sh
@@ -6,4 +6,4 @@ cd /root/projects/kogito
 clear
 
 # Clone our Coffee service
-git clone git@github.com:DuncanDoyle/coffeeservice-quarkus.git /root/projects/kogito
+git clone https://github.com/DuncanDoyle/coffeeservice-quarkus.git /root/projects/kogito
