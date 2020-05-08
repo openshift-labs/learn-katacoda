65f1fa8ea89e998c2d36074d67a2889439aaaab1
Duncan Doyle <Duncan.Doyle@gmail.com>
2020-05-08 09:19:15 +0200

    Cloning CoffeeService into workspace.

--- middleware/middleware-kogito/rest-service-integration/set-env.sh
+++ middleware/middleware-kogito/rest-service-integration/set-env.sh
@@ -4,3 +4,6 @@ export PATH=$GRAALVM_HOME/bin:$PATH
 mkdir -p /root/projects/kogito
 cd /root/projects/kogito
 clear
+
+# Clone our Coffee service
+git clone git@github.com:DuncanDoyle/coffeeservice-quarkus.git /root/projects/kogito
