4dbae1aa4881eb8efc2c69568a36f118046f737d
weimeilin79 <weimielin@gmail.com>
2020-05-13 16:16:36 -0400

    update to nightly build

--- middleware/middleware-camelk/camel-k-serving/env-init.sh
+++ middleware/middleware-camelk/camel-k-serving/env-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/apache/camel-k/releases/download/1.0.0-RC2/camel-k-client-1.0.0-RC2-linux-64bit.tar.gz
-tar -xvf camel-k-client-1.0.0-RC2-linux-64bit.tar.gz
+wget https://github.com/apache/camel-k/releases/download/1.0.0-nightly.202005130003/camel-k-client-1.0.0-nightly.202005130003-linux-64bit.tar.gz
+tar -xvf camel-k-client-1.0.0-nightly.202005130003-linux-64bit.tar.gz
 mv kamel /usr/bin
 
 
