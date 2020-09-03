955c18a243a5b388a692f5e14bc226a115e10f57
Brian Tannous <brian@briantannous.com>
2020-09-03 11:18:41 -0500

    OpenShift 4.5 Environment: Update Client Packages

--- environments/openshift-4-5/client/build/1_packages.sh
+++ environments/openshift-4-5/client/build/1_packages.sh
@@ -2,44 +2,59 @@ yum install -y nc
 yum install -y vim man curl wget unzip zip git zsh tmux java-1.8.0-openjdk-devel tree git bash-completion net-tools python36 glibc-langpack-en rsync
 
 export MVN_VERSION=3.6.3
-export KAMEL_VERSION=1.0.0-RC2
-export KOGITO_VERSION=0.10.0
+export KUBECTL_VERSION=v1.19.0
+export OC_VERSION=4.5
+export CAMEL_K_VERSION=1.0
+export HELM_VERSION=3.2.3
+export ODO_VERSION=1.2.5
+export TKN_VERSION=0.11.0
+export KN_VERSION=0.14.0
+export KOGITO_VERSION=0.14.0
 
-RUN wget http://www.eu.apache.org/dist/maven/maven-3/$MVN_VERSION/binaries/apache-maven-$MVN_VERSION-bin.tar.gz && \
-	tar xzf apache-maven-$MVN_VERSION-bin.tar.gz && \
-	rm -rf apache-maven-$MVN_VERSION-bin.tar.gz && \
-	mkdir /usr/local/maven && \
-	mv apache-maven-$MVN_VERSION/ /usr/local/maven/ && \
-	alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-$MVN_VERSION/bin/mvn 1
+wget http://www.eu.apache.org/dist/maven/maven-3/$MVN_VERSION/binaries/apache-maven-$MVN_VERSION-bin.tar.gz && \
+    tar xzf apache-maven-$MVN_VERSION-bin.tar.gz && \
+    rm -rf apache-maven-$MVN_VERSION-bin.tar.gz && \
+    mkdir -p /usr/local/maven && \
+    mv apache-maven-$MVN_VERSION/ /usr/local/maven/ && \
+    alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-$MVN_VERSION/bin/mvn 1
 
-curl -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.17.1/bin/linux/amd64/kubectl && chmod a+x /usr/local/bin/kubectl
+curl -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && chmod a+x /usr/local/bin/kubectl
 
-curl -o oc.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.4/linux/oc.tar.gz && \
+curl -o oc.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/oc/$OC_VERSION/linux/oc.tar.gz && \
     tar -xvf oc.tar.gz && \
     rm -f oc.tar.gz && \
     mv oc /usr/bin/oc && \
     chmod +x /usr/bin/oc
 
-curl -o odo.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/v1.2.1/odo-linux-amd64.tar.gz && \
+curl -o camel_k.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/camel-k/$CAMEL_K_VERSION/camel-k-client-$CAMEL_K_VERSION-linux-64bit.tar.gz && \
+    tar -xvf camel_k.tar.gz && \
+    rm -f camel_k.tar.gz kamel.asc kamel.sha512 && \
+    mv kamel /usr/bin/kamel && \
+    chmod +x /usr/bin/kamel
+
+curl -o /usr/local/bin/helm https://mirror.openshift.com/pub/openshift-v4/clients/helm/$HELM_VERSION/helm-linux-amd64 && chmod a+x /usr/local/bin/helm
+
+curl -o odo.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/v$ODO_VERSION/odo-linux-amd64.tar.gz && \
     tar -xvf odo.tar.gz && \
     rm -f odo.tar.gz && \
     mv odo /usr/bin/odo && \
     chmod +x /usr/bin/odo
 
-curl -o kn.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/serverless/0.13.2/kn-linux-amd64-0.13.2.tar.gz && \
+curl -o tkn.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/pipeline/$TKN_VERSION/tkn-linux-amd64-$TKN_VERSION.tar.gz && \
+    tar -xvf tkn.tar.gz && \
+    rm -f tkn.tar.gz && \
+    mv tkn /usr/bin/tkn && \
+    chmod +x /usr/bin/tkn
+
+curl -o kn.tar.gz -L https://mirror.openshift.com/pub/openshift-v4/clients/serverless/$KN_VERSION/kn-linux-amd64-$KN_VERSION.tar.gz && \
     tar -xvf kn.tar.gz && \
     rm -f kn.tar.gz && \
     mv kn /usr/bin/kn && \
     chmod +x /usr/bin/kn
 
-curl -o kamel.tar.gz -L https://github.com/apache/camel-k/releases/download/$KAMEL_VERSION/camel-k-client-$KAMEL_VERSION-linux-64bit.tar.gz && \
-    tar -xvf kamel.tar.gz && \
-    rm -f kamel.tar.gz && \
-    mv kamel /usr/bin/kamel && \
-    chmod +x /usr/bin/kamel
-
-curl -o kogito.tar.gz -L https://github.com/kiegroup/kogito-cloud-operator/releases/download/$KOGITO_VERSION/kogito-cli-$KOGITO_VERSION-linux-amd64.tar.gz && \
+curl -o kogito.tar.gz -L https://github.com/kiegroup/kogito-cloud-operator/releases/download/v$KOGITO_VERSION/kogito-cli-$KOGITO_VERSION-linux-amd64.tar.gz && \
     tar -xvf kogito.tar.gz && \
     rm -f kogito.tar.gz && \
     mv kogito /usr/bin/kogito && \
-    chmod +x /usr/bin/kogito    
+    chmod +x /usr/bin/kogito
+
