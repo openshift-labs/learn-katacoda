0c9bd9ba8a57d5457776793ed4381271c3c3b6af
weimeilin79 <weimielin@gmail.com>
2020-10-08 09:05:35 -0400

    update operator check

--- middleware/middleware-camelk/camel-k-eventing/assets/serverless/install-serverless.sh
+++ middleware/middleware-camelk/camel-k-eventing/assets/serverless/install-serverless.sh
@@ -12,21 +12,36 @@ function wait_for_operator_install {
   local sub=$1
   while : ;
   do
+  	sleep 10
     echo "$A: Checking..."
     phase=`oc get csv -n openshift-operators $sub -o jsonpath='{.status.phase}'`
     if [[ $phase == "Succeeded" ]]; then echo "$sub Installed"; break; fi
     A=$((A+1))
-    sleep 10
   done
 }
 
-# Get serverless-operator.v.X.X... name
-# Will be stored in "${BASH_REMATCH[0]}"
-ops=`oc get csv -n openshift-operators`
-pat='(serverless-operator\S+)'
-[[ $ops =~ $pat ]] # From this line
+SERVERLESS_OP_NAME=""
 
-wait_for_operator_install ${BASH_REMATCH[0]}
+function install_operator {
+  echo SERVERLESS_OP_NAME ${SERVERLESS_OP_NAME}
+  
+  while [ -z $SERVERLESS_OP_NAME ] ;
+  do
+  	sleep 10
+  	ops=`oc get csv -n openshift-operators`
+  	pat='(serverless-operator\S+)'
+  	[[ $ops =~ $pat ]] # From this line
+    SERVERLESS_OP_NAME=${BASH_REMATCH[0]}
+    install_operator
+  done
+  
+}
+
+install_operator
+wait_for_operator_install $SERVERLESS_OP_NAME
+
+
+
 
 
 oc new-project knative-serving
@@ -38,12 +53,12 @@ oc apply -f https://raw.githubusercontent.com/weimeilin79/learn-katacoda/master/
 A=1
 while : ;
 do
+  sleep 3
   output=`oc get knativeserving.operator.knative.dev/knative-serving -n knative-serving --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}'`
   echo "$A: $output"
   if [ -z "${output##*'Ready=True'*}" ] ; then echo "Installed"; break; fi;
   A=$((A+1))
-  sleep 10
 done
 
 
-echo "Camel K Eventing Tutorial Ready!"
+echo "Camel K Eventing Tutorial Ready!"
