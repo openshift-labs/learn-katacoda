d01a019cda7fe8443c1fe25448d875a1917ad6fb
weimeilin79 <weimielin@gmail.com>
2020-10-08 08:09:34 -0400

    back to use env-init for Kamel download

--- middleware/middleware-camelk/camel-k-basic/env-init.sh
+++ middleware/middleware-camelk/camel-k-basic/env-init.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+wget https://github.com/apache/camel-k/releases/download/v1.1.1/camel-k-client-1.1.1-linux-64bit.tar.gz
+tar -xvf camel-k-client-1.1.1-linux-64bit.tar.gz
+mv kamel /usr/bin
+
+
+
+
--- middleware/middleware-camelk/camel-k-serving/env-init.sh
+++ middleware/middleware-camelk/camel-k-serving/env-init.sh
@@ -4,4 +4,4 @@ wget https://github.com/apache/camel-k/releases/download/v1.1.0/camel-k-client-1
 tar -xvf camel-k-client-1.1.0-linux-64bit.tar.gz
 mv kamel /usr/bin
 
-mkdir camel-api
+echo "Camel K Serving Tutorial Ready!"
