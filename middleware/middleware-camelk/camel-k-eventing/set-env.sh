7087bb8a0bdd07b12eaf9f2e1f5760a56ee595a8
weimeilin79 <weimielin@gmail.com>
2020-10-07 23:28:36 -0400

    update eventing to use set-env

--- middleware/middleware-camelk/camel-k-eventing/set-env.sh
+++ middleware/middleware-camelk/camel-k-eventing/set-env.sh
@@ -4,17 +4,3 @@
 
 
 
-oc apply -f https://raw.githubusercontent.com/weimeilin79/learn-katacoda/master/middleware/middleware-camelk/camel-k-eventing/assets/serverless/subscription.yaml
-
-
-
-oc new-project knative-serving
-
-sleep 10
-
-oc apply -f https://raw.githubusercontent.com/weimeilin79/learn-katacoda/master/middleware/middleware-camelk/camel-k-eventing/assets/serverless/serving.yaml -n knative-serving
-
-
-
-
-echo "Camel K Eventing Tutorial Ready!"
