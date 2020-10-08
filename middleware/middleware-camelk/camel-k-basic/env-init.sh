8c0ab7a5189fce4cc74657d80fcceb6a8657d4ea
weimeilin79 <weimielin@gmail.com>
2020-10-07 21:57:54 -0400

    Moving from env-init.sh to  set-env.sh

--- middleware/middleware-camelk/camel-k-basic/env-init.sh
+++ middleware/middleware-camelk/camel-k-basic/env-init.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-wget https://github.com/apache/camel-k/releases/download/v1.1.0/camel-k-client-1.1.0-linux-64bit.tar.gz
-tar -xvf camel-k-client-1.1.0-linux-64bit.tar.gz
-mv kamel /usr/bin
-mkdir camel-basic
