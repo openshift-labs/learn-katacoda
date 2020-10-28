922dd2e0f5a35503f79876932f34862c497b320d
Scott McCarty <scott.mccarty@gmail.com>
2020-10-28 19:12:21 -0400

    SM: removed oc comment. Not needed in this lab.

--- subsystems/container-internals-lab-2-0-part-6/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-6/foreground.sh
@@ -13,5 +13,3 @@ podman pull quay.io/fatherlinux/linux-container-internals-2-0-introduction
 skopeo copy containers-storage:registry.access.redhat.com/ubi7/ubi:latest docker-daemon:registry.access.redhat.com/ubi7/ubi:latest
 skopeo copy containers-storage:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest docker-daemon:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest
 echo "Container host is now ready."
-
-/usr/bin/oc adm policy add-cluster-role-to-user cluster-admin admin
