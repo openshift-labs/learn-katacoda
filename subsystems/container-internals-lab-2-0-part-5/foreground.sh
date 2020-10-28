5bb71ed8bee3c09285254317257da0e566eecdee
Scott McCarty <scott.mccarty@gmail.com>
2020-10-28 14:38:31 -0400

    SM: fixed typo

--- subsystems/container-internals-lab-2-0-part-5/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-5/foreground.sh
@@ -7,7 +7,7 @@ cp -f ~/labs/mega-proc.sh /usr/bin/mega-proc.sh
 podman pull registry.access.redhat.com/ubi7/ubi
 git clone --quiet --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/wordpress-demo
 ~/labs/wordpress-demo/create.sh
-sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/wordpres-demo/wordpress-objects.yaml
+sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/wordpress-demo/wordpress-objects.yaml
 git clone --quiet --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/container-supply-chain/
 oc adm policy add-role-to-user cluster-admin admin
 #systemctl restart iptables
