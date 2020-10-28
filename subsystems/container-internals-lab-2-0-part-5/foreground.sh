e27fcb064e60f63980908089ed74cd8ebced7275
Scott McCarty <scott.mccarty@gmail.com>
2020-10-28 14:33:07 -0400

    SM: make git clones quiet

--- subsystems/container-internals-lab-2-0-part-5/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-5/foreground.sh
@@ -1,14 +1,14 @@
 # General Preparation
 yum install -y podman buildah skopeo tree httpd-tools strace
-git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
+git clone --quiet --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
 cp -f ~/labs/mega-proc.sh /usr/bin/mega-proc.sh
 
 # Lab 5
 podman pull registry.access.redhat.com/ubi7/ubi
-git clone --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/wordpress-demo
+git clone --quiet --depth 1 --single-branch --branch lab4-step1 https://github.com/fatherlinux/wordpress-demo.git ~/labs/wordpress-demo
 ~/labs/wordpress-demo/create.sh
 sed -i s/wpfrontend-wordpress.apps.example.com/`hostname`/ ~/labs/wordpres-demo/wordpress-objects.yaml
-git clone --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/container-supply-chain/
+git clone --quiet --depth 1 --single-branch --branch centos7 https://github.com/fatherlinux/container-supply-chain.git ~/labs/container-supply-chain/
 oc adm policy add-role-to-user cluster-admin admin
 #systemctl restart iptables
 echo "Container host is now ready."
