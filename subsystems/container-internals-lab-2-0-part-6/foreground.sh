be1bcdbd1c3553ecb51f2f4a0ab244cd3224762c
Scott McCarty <scott.mccarty@gmail.com>
2020-10-28 21:24:37 -0400

    SM: added jq to show pretty output

--- subsystems/container-internals-lab-2-0-part-6/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-6/foreground.sh
@@ -1,7 +1,6 @@
 # General Preparation
-# General Preparation
 git clone --quiet --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install --quiet -y podman buildah skopeo skopeo-containers tree httpd-tools strace
+yum install --quiet -y podman buildah skopeo skopeo-containers tree httpd-tools strace jq
 
 # Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
