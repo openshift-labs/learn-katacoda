7d4b64d09b2d7e581bed4a72a193ab5367749a41
Scott McCarty <scott.mccarty@gmail.com>
2020-11-01 10:36:53 -0500

    SM: removed unneeded prep

--- subsystems/container-internals-lab-2-0-part-7/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-7/foreground.sh
@@ -4,7 +4,4 @@ yum install --quiet -y podman buildah skopeo skopeo-containers tree httpd-tools 
 
 # Lab 1
 podman pull registry.access.redhat.com/ubi7/ubi
-podman pull quay.io/fatherlinux/linux-container-internals-2-0-introduction
-skopeo copy containers-storage:registry.access.redhat.com/ubi7/ubi:latest docker-daemon:registry.access.redhat.com/ubi7/ubi:latest
-skopeo copy containers-storage:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest docker-daemon:quay.io/fatherlinux/linux-container-internals-2-0-introduction:latest
 echo "Container host is now ready."
