3480d4fd1d23061a0dc9ce7fdc011018a959aee3
Scott McCarty <scott.mccarty@gmail.com>
2020-11-01 13:40:02 -0500

    SM: added Docker repos

--- subsystems/container-internals-lab-2-0-part-7/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-7/foreground.sh
@@ -1,8 +1,7 @@
 # General Preparation
 git clone --quiet --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install --quiet -y podman buildah skopeo skopeo-containers tree httpd-tools strace jq
-
-# Lab 1
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 podman pull registry.access.redhat.com/ubi8/ubi-minimal
 podman pull registry.access.redhat.com/ubi8/ubi
 podman pull registry.access.redhat.com/ubi8/ubi-init
