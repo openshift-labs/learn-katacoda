5f12304f39a1c0dd2b05b0212ceb791e5b4a6a0e
Scott McCarty <scott.mccarty@gmail.com>
2020-11-02 13:56:31 -0500

    SM: added OSCAP prep work

--- subsystems/container-internals-lab-2-0-part-7/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-7/foreground.sh
@@ -5,4 +5,10 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 podman pull registry.access.redhat.com/ubi8/ubi-minimal
 podman pull registry.access.redhat.com/ubi8/ubi
 podman pull registry.access.redhat.com/ubi8/ubi-init
+podman pull registry.access.redhat.com/ubi8/ubi:8.0-126
+podman pull registry.access.redhat.com/rhscl/nginx-114-rhel7
+
+# Start container for OSCAP reports
+mkdir ~/html
+podman run -p 80:8080 -id --name nginx -v /root/html:/opt/app-root/src/:Z registry.access.redhat.com/rhscl/nginx-114-rhel7 nginx -g 'daemon off;'
 echo "Container host is now ready."
