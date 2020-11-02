bbaaa9f85bb1d8dedceef527a76dc8af9a354de4
Scott McCarty <scott.mccarty@gmail.com>
2020-11-02 12:32:20 -0500

    SM: install oscap tools

--- subsystems/container-internals-lab-2-0-part-7/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-7/foreground.sh
@@ -1,6 +1,6 @@
 # General Preparation
 git clone --quiet --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install -y yum-utils
+yum install -y yum-utils setools-console openscap-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 podman pull registry.access.redhat.com/ubi8/ubi-minimal
 podman pull registry.access.redhat.com/ubi8/ubi
