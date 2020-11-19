e5ee876f1f2a50be0a24dd2a8a0865797daaa807
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2020-11-19 17:06:43 -0500

    fix command

--- middleware/middleware-kafka/kafka-debezium/env-init.sh
+++ middleware/middleware-kafka/kafka-debezium/env-init.sh
@@ -5,7 +5,7 @@ oc -n openshift-operators apply -f /opt/operator-install.yaml
 
 # Grant permission to developer
 oc apply -f /opt/strimzi-admin.yaml
-oc adm add-role-to-user strimzi-admin developer
+oc adm policy add-cluster-role-to-user strimzi-admin developer
 
 # Start pre-populated database
 oc new-app --name=mysql -e MYSQL_ROOT_PASSWORD=debezium -e MYSQL_USER=mysqluser -e MYSQL_PASSWORD=mysqlpw debezium/example-mysql:1.3
