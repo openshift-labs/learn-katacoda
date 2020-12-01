6b110428b102295c7eb836980dc5d17585a9d990
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2020-11-30 19:39:55 -0500

    wait for crd to be installed

--- middleware/middleware-kafka/kafka-http-bridge/env-init.sh
+++ middleware/middleware-kafka/kafka-http-bridge/env-init.sh
@@ -5,9 +5,21 @@ oc -n openshift-operators apply -f /opt/operator-install.yaml
 
 # Start Kafka
 oc new-project kafka
+
+# Check if operator is installed
+oc get crds kafkas.kafka.strimzi.io
+
+while [ $? -ne 0 ]; do
+    oc get crds kafkas.kafka.strimzi.io
+    sleep 2
+done
+
+# Deploy cluster
 oc -n kafka apply -f /opt/kafka-cluster.yaml
 
 # Grant permission to developer
 oc apply -f /opt/strimzi-admin.yaml
+
 oc adm policy add-cluster-role-to-user strimzi-admin developer
+
 oc adm policy add-role-to-user admin developer -n kafka
