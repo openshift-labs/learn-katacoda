ad9de8f816e51f268e25b538591773ce56561263
Natale Vinto <nvinto@redhat.com>
2021-06-03 18:20:28 +0200

    Fix paths

--- developing-on-openshift/helm/set-env.sh
+++ developing-on-openshift/helm/set-env.sh
@@ -2,6 +2,5 @@
 # This is the script to put provisioning things in, this runs in the foreground
 set -o pipefail
 mkdir -p /root/helm/my-chart/charts
-cd /root/helm
 clear
 echo "Helm Tutorial Ready!" 
