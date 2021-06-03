e804ae9e7d31aed9c936a0f6349e7d0cfb7726ac
Natale Vinto <nvinto@redhat.com>
2021-06-03 17:55:51 +0200

    Change dir after

--- developing-on-openshift/helm/set-env.sh
+++ developing-on-openshift/helm/set-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is the script to put provisioning things in, this runs in the foreground
 set -o pipefail
-cd /root/helm
 mkdir -p /root/helm/my-chart/charts
+cd /root/helm
 clear
 echo "Helm Tutorial Ready!" 
