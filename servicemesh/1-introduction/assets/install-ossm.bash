63dbc4e218c1d80a7621aeca394835d4d7b5093d
Brian Tannous <Brian@BrianTannous.com>
2021-06-23 23:44:06 -0500

    ossm script output cleanup

--- servicemesh/1-introduction/assets/install-ossm.bash
+++ servicemesh/1-introduction/assets/install-ossm.bash
@@ -186,3 +186,4 @@ oc apply -n ${BOOKINFO_NS} -f https://raw.githubusercontent.com/Maistra/istio/ma
 oc apply -n ${BOOKINFO_NS} -f https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/networking/bookinfo-gateway.yaml
 export GATEWAY_URL=$(oc get -n ${CONTROL_PLANE_NS} route istio-ingressgateway -o jsonpath='{.spec.host}')
 oc apply -n ${BOOKINFO_NS} -f https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/networking/destination-rule-all.yaml
+echo "Try bookinfo out by using: \`curl ${GATEWAY_URL}/productpage\`"
