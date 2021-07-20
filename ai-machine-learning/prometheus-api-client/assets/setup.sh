e0d2972b850b17d7cb3ca03fba4bc29139188054
Natale Vinto <nvinto@redhat.com>
2021-07-20 16:50:48 +0200

    Fix permission issue
    Fixes https://github.com/openshift-labs/learn-katacoda/issues/1246

--- ai-machine-learning/prometheus-api-client/assets/setup.sh
+++ ai-machine-learning/prometheus-api-client/assets/setup.sh
@@ -13,7 +13,12 @@ curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.
 
 # set up Notebooks
 oc process -f ./notebook-imagestream.yaml | oc apply -f - -n myproject
+# Workaround permission issue in container image quay.io/hveeradh/prometheus-anomaly-detection-workshop
+oc login -u admin -p admin --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
+oc adm policy add-scc-to-user anyuid -z default -n myproject
+# Deploy Notebooks
 oc process notebook-deployer -p APPLICATION_NAME=prometheus-anomaly-detection-workshop -p NOTEBOOK_IMAGE=prometheus-anomaly-detection-workshop:prometheus-api-client-katacoda -p NOTEBOOK_PASSWORD=secret | oc apply -f - -n myproject
+oc login -u developer -p developer --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
 clear
 echo -e "Waiting for metrics data to be generated... (This might take a couple minutes)"
 until [ "$(oc get job prometheus-generate-data -o jsonpath='{.status.succeeded}' -n myproject &)" = "1" ];
