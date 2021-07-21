04a486147bd60af4d6bb642c382b8408d4da074a
Natale Vinto <nvinto@redhat.com>
2021-07-21 17:08:40 +0200

    Update setup.sh

--- ai-machine-learning/prometheus-api-client/assets/setup.sh
+++ ai-machine-learning/prometheus-api-client/assets/setup.sh
@@ -1,20 +1,18 @@
-set +x
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
 oc login -u developer -p developer --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
 
 oc new-project myproject
 
 # set up dummy metrics in PVC
-oc process -f ./generate-metrics.yaml | oc apply -n myproject -f -
+oc process -f ./generate-metrics.yaml | oc apply -n myproject -f - --as system:admin
 # set up Prometheus
-oc process -f ./deploy-prometheus.yaml | oc apply -n myproject -f -
+oc process -f ./deploy-prometheus.yaml | oc apply -n myproject -f - --as system:admin
 
 curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.4.0/templates/notebook-deployer.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
 
 # set up Notebooks
 oc process -f ./notebook-imagestream.yaml | oc apply -f - -n myproject
 # Workaround permission issue in container image quay.io/hveeradh/prometheus-anomaly-detection-workshop
-sleep 10
 oc login -u admin -p admin --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
 oc adm policy add-scc-to-user anyuid -z default -n myproject
 # Deploy Notebooks
@@ -27,14 +25,8 @@ until [ "$(oc get job prometheus-generate-data -o jsonpath='{.status.succeeded}'
 do
   sleep 10
 done
+oc adm policy remove-scc-from-user anyuid -z default -n myproject
 clear
 # set up Prometheus
 echo -e "Metric data generated, Setting up Prometheus"
 oc rollout latest prometheus-demo
-
-
-oc logs bc/prometheus-anomaly-detection-workshop -f
-clear
-echo -e "The environment should be ready in a few seconds"
-echo -e "The url to access the Jupyter Notebooks is: \n https://$(oc get route prometheus-anomaly-detection-workshop -o jsonpath='{.spec.host}' -n myproject) \n\n"
-echo -e "Prometheus Console is available at: \n http://$(oc get route prometheus-demo-route -o jsonpath='{.spec.host}' -n myproject)"
