c6dfa7a1f6d2dd14e05780247cc63863d02eb4c8
Natale Vinto <nvinto@redhat.com>
2021-07-22 16:43:01 +0200

    Update to openshift-katacoda

--- ai-machine-learning/prometheus-api-client/assets/setup.sh
+++ ai-machine-learning/prometheus-api-client/assets/setup.sh
@@ -8,7 +8,7 @@ oc process -f ./generate-metrics.yaml | oc apply -n myproject -f - --as system:a
 # set up Prometheus
 oc process -f ./deploy-prometheus.yaml | oc apply -n myproject -f - --as system:admin
 
-curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.4.0/templates/notebook-deployer.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/openshift-katacoda/ai-machine-learning-jupyter-notebooks/2.4.0/templates/notebook-deployer.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
 
 # set up Notebooks
 oc process -f ./notebook-imagestream.yaml | oc apply -f - -n myproject
