56af1d52c0a986b073c33361a1221ac4d22da581
Scott McCarty <scott.mccarty@gmail.com>
2021-11-01 22:08:26 -0400

    SM: modified to fix issue: https://github.com/openshift-labs/learn-katacoda/issues/1324

--- subsystems/container-internals-lab-2-0-part-3/foreground.sh
+++ subsystems/container-internals-lab-2-0-part-3/foreground.sh
@@ -1,10 +1,7 @@
 # General Preparation
 git clone --depth 1 https://github.com/fatherlinux/container-internals-lab.git ~/labs
-yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace
-yum install -y https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/noarch/podman-manpages-1.5.1-3.el7.noarch.rpm \
-https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/x86_64/podman-1.5.1-3.el7.x86_64.rpm \
-https://cbs.centos.org/kojifiles/packages/slirp4netns/0.3.0/2.git4992082.el7/x86_64/slirp4netns-0.3.0-2.git4992082.el7.x86_64.rpm \
-https://cbs.centos.org/kojifiles/packages/libseccomp/2.4.1/0.el7/x86_64/libseccomp-2.4.1-0.el7.x86_64.rpm
+yum module install -y container-tools:rhel8
+yum install -y tree httpd-tools strace
 cp -f ~/labs/lab1-step3/mega-proc.sh /usr/bin/mega-proc.sh
 
 # Lab 3
